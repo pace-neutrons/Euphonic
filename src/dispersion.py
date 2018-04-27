999e36fa96937a2cbd09da5ae3a0f84e49916e9b
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2018-04-27 13:20:01 +0100

    Add q-point distance (abscissa) calculation

--- src/dispersion.py
+++ src/dispersion.py
@@ -2,12 +2,17 @@
 Parse a *.castep, *.phonon or *.band output file from new CASTEP for vibrational frequency 
 data and output a matplotlib plot of the electronic or vibrational band structure or dispersion.
 """
+
 import numpy as np
+import math
 import argparse
 
 def main():
     args = parse_arguments()
-    read_dot_bands(args.filename)
+    freq_up, freq_down, kpts, fermi, weights, cell = read_dot_bands(args.filename)
+
+    recip_latt = reciprocal_lattice(cell)
+    abscissa = calc_abscissa(kpts, recip_latt)
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="""Extract phonon or bandstructure data
@@ -48,7 +53,7 @@ def read_dot_bands(bands_file):
     freq_down : list of floats
         M x N list of spin down band frequencies in eV, where M = number of k-points and
         N = number of bands, ordered according to increasing k-point number
-    qpts: list of floats
+    kpts: list of floats
         M x 3 list of k-point coordinates, where M = number of k-points
     fermi: list of floats
         List of length 1 or 2 containing the Fermi energy/energies in atomic units
@@ -68,13 +73,13 @@ def read_dot_bands(bands_file):
 
     freq_up = np.zeros((n_kpts, n_freqs))
     freq_down = np.zeros((n_kpts, n_freqs))
-    qpts = np.zeros((n_kpts, 3))
+    kpts = np.zeros((n_kpts, 3))
     weights = np.zeros(n_kpts)
 
     for i in range(n_kpts):
         line = f.readline().split()
         kpt = int(line[1]) - 1
-        qpts[kpt,:] = [float(x) for x in line[2:5]]
+        kpts[kpt,:] = [float(x) for x in line[2:5]]
         weights[kpt] = float(line[5])
 
         for j in range(n_spins):
@@ -88,6 +93,61 @@ def read_dot_bands(bands_file):
                     freq_down[kpt, k] = freq
 
     f.close()
-    return freq_up, freq_down, qpts, fermi, weights, cell
+    return freq_up, freq_down, kpts, fermi, weights, cell
+
+def calc_abscissa(qpts, recip_latt):
+    """
+    Calculates the abscissa (x-coordinate) for every q-point for plotting,
+    based on the distance between q-points
+    """
+
+    KV_TOL = 0.001
+
+    # Get difference between q-points. Note: length is nqpts - 1
+    delta = np.diff(qpts, axis=0)
+    # Get sum of differences between delta and nearest integer for each q-point
+    delta_rem = np.sum(np.abs(delta - np.rint(delta)), axis=1)
+
+    # Determine whether to calculate modq based on tolerances
+    calc_modq = np.logical_not(np.logical_and(np.sum(np.abs(delta), axis=1) > KV_TOL, delta_rem < KV_TOL))
+
+    # Multiply each delta by the reciprocal lattice to get delta in cartesian coords
+    deltaq = np.einsum('ji,kj->ki', recip_latt, delta)
+
+    # Get distance between qpoints for all valid pairs of qpoints
+    modq = np.zeros(np.size(delta, axis=0))
+    modq[calc_modq] = np.sqrt(np.sum(np.square(deltaq), axis=1))
+
+    # Prepend initial x axis value of 0
+    abscissa = np.insert(modq, 0, 0.)
+
+    # Do cumulative some to get position along x axis
+    abscissa = np.cumsum(abscissa)
+
+    return abscissa
+
+def reciprocal_lattice(unit_cell):
+    """
+    Calculates the reciprocal lattice from a unit cell
+    """
+    print(np.array(unit_cell))
+    print(np.linalg.det(unit_cell))
+
+    a = np.array(unit_cell[0])
+    b = np.array(unit_cell[1])
+    c = np.array(unit_cell[2])
+
+    bxc = np.cross(b,c)
+    cxa = np.cross(c,a)
+    axb = np.cross(a,b)
+
+    adotbxc = np.vdot(a,bxc) # Unit cell volume
+    norm = 2*math.pi/adotbxc # Normalisation factor
+
+    astar = norm*bxc
+    bstar = norm*cxa
+    cstar = norm*axb
+
+    return np.array([astar,bstar,cstar])
 
 main()
