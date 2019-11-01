77e9fa4d66c3feb14fc8a9f220eb266eb31e1f89
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2019-11-01 14:01:21 +0000

    Update setup.py and README

--- README.md
+++ README.md
@@ -4,24 +4,5 @@ inelastic neutron scattering intensities from modelling code outputs (e.g.
 CASTEP). In addition, it can also do simple plotting, and can also plot
 dispersion and density of states from existing CASTEP .bands or .phonon files.
 
-## Installation
-The easiest way to install the Euphonic package is using `pip`. First clone
-this repository and cd into the top directory containing the `setup.py` script.
-
-This package does all plotting with Matplotlib, to install Euphonic with the
-optional Matplotlib dependency do:
-```
-pip install .[matplotlib]
-```
-Or to install for just your user:
-```
-pip install --user .[matplotlib]
-```
-If you only require the interpolation functionality, and don't need any of the
-Matplotlib plotting routines just do:
-```
-pip install .
-```
-
 ## Usage
 For tutorials and more information, see the [Wiki](https://github.com/pace-neutrons/Euphonic/wiki)
--- setup.py
+++ setup.py
@@ -3,12 +3,30 @@ try:
 except ImportError:
     from distutils.core import setup, find_packages
 
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+packages = ['euphonic',
+            'euphonic.data',
+            'euphonic.plot',
+            'euphonic._readers']
+
+scripts = ['scripts/dispersion.py',
+           'scripts/dos.py',
+           'scripts/optimise_eta.py']
+
 setup(
-    name='Euphonic',
-    version='0.1dev',
-    description="""Module to read CASTEP electronic/vibrational frequency data
-                   and output a dispersion/dos plot""",
-    packages=find_packages(),
+    name='euphonic',
+    version='0.1dev2',
+    author='Rebecca Fair',
+    author_email='rebecca.fair@stfc.ac.uk',
+    description=(
+        'Euphonic calculates phonon bandstructures and inelastic neutron '
+        'scattering intensities from modelling code outputs (e.g. CASTEP)'),
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/pace-neutrons/Euphonic',
+    packages=packages,
     install_requires=[
         'numpy>=1.9.1',
 	'scipy>=1.0.0',
@@ -16,6 +34,7 @@ setup(
         'pint>=0.8.0'
     ],
     extras_require={
-        'matplotlib': ['matplotlib>=1.4.2']
-    }
+        'matplotlib': ['matplotlib>=1.4.2'],
+    },
+    scripts=scripts
 )
