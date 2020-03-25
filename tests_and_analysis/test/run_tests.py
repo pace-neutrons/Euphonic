ec1b5934b6e00273484c2e50b76e45993ebe6058
JamesKingWork <JamesKingWork@ndlt1208.isis.cclrc.ac.uk>
2020-03-25 12:13:28 +0000

    Correct coverage merge

--- tests_and_analysis/test/run_tests.py
+++ tests_and_analysis/test/run_tests.py
@@ -1,27 +1,26 @@
 import sys
 import os
-import unittest
-import xmlrunner
+import pytest
 import coverage
 
 if __name__ == "__main__":
 
+    # Set output directory to the reports directory under this file's directory
+
     # Discover tests
     test_dir = os.path.dirname(os.path.abspath(__file__))
-    os.chdir(test_dir)
-    test_suite = unittest.TestLoader().discover(test_dir, pattern="test_*.py")
+    xml_filepath = os.path.join(test_dir, "reports", "junit_report.xml")
 
     # Start recording coverage
     cov = coverage.Coverage(config_file=".coveragerc")
     cov.start()
 
-    # Set output directory and run tests
-    high_verbosity = 2
-    xml_dir = os.path.join(test_dir, "reports")
-    ret_vals = xmlrunner.XMLTestRunner(output=xml_dir, verbosity=high_verbosity).run(test_suite)
+    # Run tests and get the resulting exit code
+    # 0 is success, 1-5 are different forms of failure (see pytest docs for details)
+    test_exit_code = pytest.main(["-x", test_dir, "--junitxml={}".format(xml_filepath)])
 
     cov.stop()
     cov.xml_report()
 
     # Exit with a failure code if there are any errors or failures
-    sys.exit(bool(ret_vals.errors or ret_vals.failures))
+    sys.exit(test_exit_code)
