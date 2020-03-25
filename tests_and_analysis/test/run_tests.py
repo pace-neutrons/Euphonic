50cd137ddbb644545b312b874b1bf461c60b7a38
JamesKingWork <JamesKingWork@ndlt1208.isis.cclrc.ac.uk>
2020-03-25 14:18:03 +0000

    Correct address for coverage.xml

--- tests_and_analysis/test/run_tests.py
+++ tests_and_analysis/test/run_tests.py
@@ -7,7 +7,9 @@ if __name__ == "__main__":
 
     # Set output directory to the reports directory under this file's directory
     test_dir = os.path.dirname(os.path.abspath(__file__))
-    xml_filepath = os.path.join(test_dir, "reports", "junit_report.xml")
+    xml_dir = os.path.join(test_dir, "reports")
+    junit_xml_filepath = os.path.join(xml_dir, "junit_report.xml")
+    coverage_xml_filepath = os.path.join(xml_dir, "coverage.xml")
 
     # Start recording coverage
     cov = coverage.Coverage(config_file=".coveragerc")
@@ -15,11 +17,11 @@ if __name__ == "__main__":
 
     # Run tests and get the resulting exit code
     # 0 is success, 1-5 are different forms of failure (see pytest docs for details)
-    test_exit_code = pytest.main(["-x", test_dir, "--junitxml={}".format(xml_filepath)])
+    test_exit_code = pytest.main(["-x", test_dir, "--junitxml={}".format(junit_xml_filepath)])
 
     # Report coverage
     cov.stop()
-    cov.xml_report()
+    cov.xml_report(outfile=coverage_xml_filepath)
 
     # Exit with a failure code if there are any errors or failures
     sys.exit(test_exit_code)
