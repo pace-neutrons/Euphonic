43e482b1994cbda59aabc9855de360e0dfd1b446
JamesKingWork <JamesKingWork@ndlt1208.isis.cclrc.ac.uk>
2020-03-30 17:03:38 +0100

    Test optimise eta selects optimal correctly

--- tests_and_analysis/test/run_tests.py
+++ tests_and_analysis/test/run_tests.py
@@ -8,8 +8,12 @@ if __name__ == "__main__":
 
     # Check whether the recording coverage has been requested
     parser = argparse.ArgumentParser()
-    parser.add_argument("--cov", action="store_true")
-    parser.add_argument("--report", action="store_true")
+    parser.add_argument("--cov", action="store_true",
+                        help="If present report coverage in a coverage.xml file in reports")
+    parser.add_argument("--report", action="store_true",
+                        help="If present report test results to junit_report*.xml files")
+    parser.add_argument("-t", "--test-file", dest="test_file", action="store",
+                        help="The test file to run", default=None)
     args_parsed = parser.parse_args()
     do_record_coverage = args_parsed.cov
     do_report_results = args_parsed.report
@@ -19,6 +23,12 @@ if __name__ == "__main__":
     xml_dir = os.path.join(test_dir, "reports")
     if not os.path.exists(xml_dir):
         os.mkdir(xml_dir)
+
+    # Handle the test file to be executed
+    if args_parsed.test_file is not None:
+        tests = os.path.join(test_dir, args_parsed.test_file)
+    else:
+        tests = test_dir
 
     # Start recording coverage if requested
     cov = None
@@ -31,9 +41,9 @@ if __name__ == "__main__":
     # 0 is success, 1-5 are different forms of failure (see pytest docs for details)
     if do_report_results:
         junit_xml_filepath = os.path.join(xml_dir, "junit_report.xml")
-        test_exit_code = pytest.main(["-x", test_dir, "--junitxml={}".format(junit_xml_filepath)])
+        test_exit_code = pytest.main(["-x", tests, "--junitxml={}".format(junit_xml_filepath)])
     else:
-        test_exit_code = pytest.main(["-x", test_dir])
+        test_exit_code = pytest.main(["-x", tests])
 
     # Report coverage if requested
     if do_record_coverage and cov is not None:
--- tests_and_analysis/test/test_optimise_eta.py
+++ tests_and_analysis/test/test_optimise_eta.py
@@ -1,7 +1,56 @@
+import os
+import math
 import pytest
+from scripts.optimise_eta import calculate_optimum_eta
+from .utils import get_data_path
+
+quartz_castep_bin = os.path.join(get_data_path(), "interpolation", "quartz", "quartz")
 
 
-def test_file1_method2():
-    x = 5
-    y = 6
-    assert x+1 == y, "test failed"
+@pytest.fixture
+def call_with_defaults_and_quartz():
+    return calculate_optimum_eta(quartz_castep_bin)
+
+
+# Regression test
+def test_optimal_is_0_75(call_with_defaults_and_quartz):
+    optimal_eta = call_with_defaults_and_quartz[0]
+    assert optimal_eta == 0.75
+
+
+# Create multiple fixtures to call with multiple selections of keyword args
+@pytest.fixture(params=[
+    {},
+    {"eta_min": 0.25, "eta_max": 1.35, "eta_step": 0.2, "n": 20},
+    {"eta_min": -0.25, "eta_max": 1.75, "eta_step": 0.2, "n": 20}
+])
+def call_with_params_and_quartz(request):
+    kwargs = request.param
+    return calculate_optimum_eta(quartz_castep_bin, **kwargs)
+
+
+def get_lowest_time_per_qpt_and_index(etas_time_per_qpts):
+    # Search etas_time_per_qpts for lowest time
+    lowest_time_per_qpt = math.inf
+    lowest_time_per_qpt_index = None
+    for index, time_per_qpt in enumerate(etas_time_per_qpts):
+        if time_per_qpt < lowest_time_per_qpt:
+            lowest_time_per_qpt = time_per_qpt
+            lowest_time_per_qpt_index = index
+    return lowest_time_per_qpt, lowest_time_per_qpt_index
+
+
+def test_optimal_has_lowest_time_per_qpt(call_with_params_and_quartz):
+    # Unpack data
+    optimal_eta = call_with_params_and_quartz[0]
+    optimal_time_per_qpt = call_with_params_and_quartz[2]
+    etas = call_with_params_and_quartz[3]
+    etas_time_per_qpts = call_with_params_and_quartz[5]
+
+    lowest_time_per_qpt, lowest_time_per_qpt_index = \
+        get_lowest_time_per_qpt_and_index(etas_time_per_qpts)
+
+    # Check optimal time per q-point is the lowest detected q-point
+    assert optimal_time_per_qpt == lowest_time_per_qpt
+    # Check eta from lowest detected q-point index matches our optimal q-point
+    assert etas[lowest_time_per_qpt_index] == optimal_eta
