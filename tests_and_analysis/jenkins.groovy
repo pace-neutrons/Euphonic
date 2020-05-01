1857ff59cc478d2e10d176509b2a67dc40b70e94
JamesKingWork <james.c.king@stfc.ac.uk>
2020-05-01 16:39:56 +0100

    Try importing libraries differently

--- tests_and_analysis/jenkins.groovy
+++ tests_and_analysis/jenkins.groovy
@@ -1,4 +1,4 @@
-def call(String status, String message, String context) {
+def setGitHubBuildStatus(String status, String message, String context) {
     script {
         withCredentials([string(credentialsId: 'Euphonic_GitHub_API_Token',
                 variable: 'api_token')]) {
@@ -32,3 +32,16 @@ def call(String status, String message, String context) {
         }
     }
 }
+
+def getGitCommitAuthorEmail() {
+    withCredentials([string(credentialsId: 'Euphonic_GitHub_API_Token',
+            variable: 'api_token')]) {
+        String GITHUB_API_BRANCH_URL = "https://api.github.com/repos/pace-neutrons/Euphonic/git/ref/heads/${env.JOB_BASE_NAME}"
+        def branch_url_response = GITHUB_API_BRANCH_URL.toURL().getText(requestProperties: ['Authorization': "token ${api_token}"])
+        def commit_url = new JsonSlurper().parseText(branch_url_response).object.url
+        def commit_url_response = commit_url.toURL().getText(requestProperties: ['Authorization': "token ${api_token}"])
+        return new JsonSlurper().parseText(commit_url_response).author.email
+    }
+}
+
+return this
