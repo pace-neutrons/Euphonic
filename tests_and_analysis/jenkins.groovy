2e22db3182a9422fe2c97e8862e4cc67817b5bb2
JamesKingWork <james.c.king@stfc.ac.uk>
2020-05-01 15:01:32 +0100

    Move getCommiAuthorEmail to groovy, put utils. calls in scripts

--- tests_and_analysis/jenkins.groovy
+++ tests_and_analysis/jenkins.groovy
@@ -34,28 +34,13 @@ def setGitHubBuildStatus(String status, String message, String context) {
 }
 
 def getGitCommitAuthorEmail() {
-    script {
-        withCredentials([string(credentialsId: 'Euphonic_GitHub_API_Token',
-                variable: 'api_token')]) {
-            if(isUnix()) {
-                return sh(
-                    script: """
-                        commit_url="\$(\\
-                            curl -s -H "Authorization: token ${api_token}" \\
-                            --request GET https://api.github.com/repos/pace-neutrons/Euphonic/git/ref/heads/${env.JOB_BASE_NAME} \\
-                            | jq ".object.url" | tr -d '"'\\
-                        )" &&
-                        echo "\$(\\
-                            curl -s -H "Authorization: token ${api_token}" \\
-                            --request GET \$commit_url |  jq '.author.email' | tr -d '"'\\
-                        )"
-                    """,
-                    returnStdout: true
-                )
-            } else {
-                error("Cannot get commit author in Windows")
-            }
-        }
+    withCredentials([string(credentialsId: 'Euphonic_GitHub_API_Token',
+            variable: 'api_token')]) {
+        String GITHUB_API_BRANCH_URL = "https://api.github.com/repos/pace-neutrons/Euphonic/git/ref/heads/${env.JOB_BASE_NAME}"
+        def branch_url_response = GITHUB_API_BRANCH_URL.toURL().getText(requestProperties: ['Authorization': "token ${api_token}"])
+        def commit_url = new JsonSlurper().parseText(branch_url_response).object.url
+        def commit_url_response = commit_url.toURL().getText(requestProperties: ['Authorization': "token ${api_token}"])
+        return new JsonSlurper().parseText(commit_url_response).author.email
     }
 }
 
