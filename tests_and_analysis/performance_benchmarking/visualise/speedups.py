5a555cb15dea803c17e05764679e496b013d0d8c
JamesKingWork <james.c.king@stfc.ac.uk>
2020-05-12 14:17:40 +0100

    Add docstring

--- tests_and_analysis/performance_benchmarking/visualise/speedups.py
+++ tests_and_analysis/performance_benchmarking/visualise/speedups.py
@@ -3,6 +3,22 @@ import matplotlib.pyplot as plt
 
 
 def plot_speedups_for_file(filename: str, figure_index: int) -> int:
+    """
+    Plot a figure for each test that has had speedups calculated for it in
+    filename. There is a trace for each seedname used in the test.
+
+    Parameters
+    ----------
+    filename : str
+        The file to get the calculated speedups from
+    figure_index : str
+        The index of the first figure to plot
+
+    Returns
+    -------
+    int
+        The next free figure index to use
+    """
     data = json.load(open(filename))
     if "speedups" in data:
         for test in data["speedups"]:
@@ -16,6 +32,8 @@ def plot_speedups_for_file(filename: str, figure_index: int) -> int:
             plt.xlabel("Number of threads")
             plt.ylabel("Speedup (Ts/Tp)")
             plt.title("Speedups for {}\n {}".format(filename, test))
+            # Create the legend to the right of the figure and shrink the
+            # figure to account for that
             plt.legend(title="Seedname/material",
                        loc='center left',
                        bbox_to_anchor=(1, 0.5))
