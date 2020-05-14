e6fba15d7deea4b9c69910502d4b0fca5e1a887b
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2020-05-14 14:03:09 +0100

    Update scripts for new API

--- tests_and_analysis/performance_benchmarking/test_benchmark_fc.py
+++ tests_and_analysis/performance_benchmarking/test_benchmark_fc.py
@@ -1,21 +1,23 @@
+import os
 import pytest
 from utils import get_data_path, get_seednames,\
     get_qpts
 
-from euphonic.data.interpolation import InterpolationData
+from euphonic import ureg, ForceConstants
 
 
 @pytest.mark.parametrize("seedname", get_seednames())
 @pytest.mark.parametrize("use_c", [True, False])
 @pytest.mark.parametrize("n_threads", [1, 2, 4, 8, 12, 16, 24])
-def test_calculate_fine_phonons(seedname, use_c, n_threads, benchmark):
+def test_calculate_qpoint_phonon_modes(seedname, use_c, n_threads, benchmark):
     # Set up
-    idata = InterpolationData.from_castep(seedname, path=get_data_path())
+    fc = ForceConstants.from_castep(
+            os.path.join(get_data_path(), f'{seedname}.castep_bin'))
     qpts = get_qpts()
     # Benchmark
     if use_c:
         benchmark(
-            idata.calculate_fine_phonons,
+            fc.calculate_qpoint_phonon_modes,
             qpts, use_c=True,
             fall_back_on_python=False,
             n_threads=n_threads,
@@ -23,7 +25,7 @@ def test_calculate_fine_phonons(seedname, use_c, n_threads, benchmark):
         )
     elif n_threads == 1:
         benchmark(
-            idata.calculate_fine_phonons,
+            fc.calculate_qpoint_phonon_modes,
             qpts, use_c=False,
             asr='reciprocal', eta_scale=0.75
         )
@@ -33,16 +35,19 @@ def test_calculate_fine_phonons(seedname, use_c, n_threads, benchmark):
 def test_calculate_structure_factor(seedname, benchmark):
     # Set up
     qpts = get_qpts()
-    idata = InterpolationData.from_castep(seedname, path=get_data_path())
-    idata.calculate_fine_phonons(
+    fc = ForceConstants.from_castep(
+            os.path.join(get_data_path(), f'{seedname}.castep_bin'))
+    phonons = fc.calculate_qpoint_phonon_modes(
         qpts, use_c=True, fall_back_on_python=False, n_threads=5
     )
+    fm = ureg('fm')
     scattering_lengths = {
-        'La': 8.24, 'Zr': 7.16, 'O': 5.803, 'C': 6.646, 'Si': 4.1491,
-        'H': -3.7390, 'N': 9.36, 'S': 2.847, 'Nb': 7.054
+        'La': 8.24*fm, 'Zr': 7.16*fm, 'O': 5.803*fm, 'C': 6.646*fm,
+        'Si': 4.1491*fm, 'H': -3.7390*fm, 'N': 9.36*fm, 'S': 2.847*fm,
+        'Nb': 7.054*fm
     }
     # Benchmark
     benchmark(
-        idata.calculate_structure_factor,
-        scattering_lengths=scattering_lengths, T=100
+        phonons.calculate_structure_factor,
+        scattering_lengths=scattering_lengths
     )
