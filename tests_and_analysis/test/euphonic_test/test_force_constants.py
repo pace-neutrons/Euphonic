dc1670e6304865e03428329eed53ab30bf0a5dd2
JamesKingWork <james.c.king@stfc.ac.uk>
2020-05-19 16:45:50 +0100

    Phonopy file reading

--- tests_and_analysis/test/euphonic_test/test_force_constants.py
+++ tests_and_analysis/test/euphonic_test/test_force_constants.py
@@ -15,3 +15,27 @@ class TestObjectCreation:
     def test_creation_from_castep(self, castep_bin_file):
         filepath = os.path.join(get_data_path(), castep_bin_file)
         ForceConstants.from_castep(filepath)
+
+    @pytest.mark.parametrize("phonopy_args", [
+        {
+            "path": os.path.join('phonopy_data', 'NaCl', 'interpolation'),
+            "fc_name": "FORCE_CONSTANTS"
+        },
+        {
+            "path": os.path.join('phonopy_data', 'NaCl', 'interpolation'),
+            "fc_name": "FULL_FORCE_CONSTANTS"
+        },
+        {
+            "path": os.path.join('phonopy_data', 'NaCl', 'interpolation'),
+            "fc_name": "full_force_constants.hdf5"
+        },
+        {
+            "path": os.path.join('phonopy_data', 'NaCl', 'interpolation'),
+            "summary_name": 'phonopy_nofc_noborn.yaml'
+        }
+    ])
+    def test_creation_from_phonopy(self, phonopy_args):
+        phonopy_args["path"] = os.path.join(
+            get_data_path(), phonopy_args["path"]
+        )
+        ForceConstants.from_phonopy(**phonopy_args)
