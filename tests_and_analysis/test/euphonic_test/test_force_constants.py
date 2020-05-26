1f442ee38ececa8cc53f9327041b65915418b7b5
JamesKingWork <james.c.king@stfc.ac.uk>
2020-05-26 09:45:20 +0100

    tidy up

--- tests_and_analysis/test/euphonic_test/test_force_constants.py
+++ tests_and_analysis/test/euphonic_test/test_force_constants.py
@@ -8,35 +8,25 @@ from ..utils import get_data_path
 class TestObjectCreation:
 
     @pytest.mark.parametrize("castep_bin_file", [
-        os.path.join('interpolation', 'LZO', 'La2Zr2O7.castep_bin'),
-        os.path.join('interpolation', 'graphite', 'graphite.castep_bin'),
-        os.path.join('interpolation', 'quartz', 'quartz.castep_bin')
+        os.path.join('LZO', 'La2Zr2O7.castep_bin'),
+        os.path.join('graphite', 'graphite.castep_bin'),
+        os.path.join('quartz', 'quartz.castep_bin')
     ])
     def test_creation_from_castep(self, castep_bin_file):
-        filepath = os.path.join(get_data_path(), castep_bin_file)
+        filepath = os.path.join(
+            get_data_path(), 'interpolation', castep_bin_file
+        )
         ForceConstants.from_castep(filepath)
 
     @pytest.mark.parametrize("phonopy_args", [
-        {
-            "path": os.path.join('phonopy_data', 'NaCl', 'interpolation'),
-            "fc_name": "FORCE_CONSTANTS"
-        },
-        {
-            "path": os.path.join('phonopy_data', 'NaCl', 'interpolation'),
-            "fc_name": "FULL_FORCE_CONSTANTS"
-        },
-        {
-            "path": os.path.join('phonopy_data', 'NaCl', 'interpolation'),
-            "fc_name": "full_force_constants.hdf5"
-        },
-        {
-            "path": os.path.join('phonopy_data', 'NaCl', 'interpolation'),
-            "summary_name": 'phonopy_nofc_noborn.yaml'
-        }
+        {"fc_name": "FORCE_CONSTANTS"},
+        {"fc_name": "FULL_FORCE_CONSTANTS"},
+        {"fc_name": "full_force_constants.hdf5"},
+        {"summary_name": "phonopy_nofc_noborn.yaml"}
     ])
     def test_creation_from_phonopy(self, phonopy_args):
         phonopy_args["path"] = os.path.join(
-            get_data_path(), phonopy_args["path"]
+            get_data_path(), 'phonopy_data', 'NaCl', 'interpolation'
         )
         ForceConstants.from_phonopy(**phonopy_args)
 
