017af58927d43f6550445dd3d15ba1037a4c5989
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2020-05-27 11:22:51 +0100

    Remove unnecessary try/catch in __init__.py
     - Version is no longer obtained by importing from __init__.py

--- euphonic/__init__.py
+++ euphonic/__init__.py
@@ -2,31 +2,24 @@ from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
 
-try:
-    # Create ureg here so it is only created once. However, this
-    # __init__.py needs to be imported by setup.py to find __version__,
-    # so allow pint import to fail as in this case pint might not be
-    # installed yet
-    from pint import UnitRegistry
-    ureg = UnitRegistry()
-    ureg.enable_contexts('spectroscopy')
-    ureg.define('@alias bohr = INTERNAL_LENGTH_UNIT')
-    ureg.define('@alias electron_mass = INTERNAL_MASS_UNIT')
-    ureg.define('@alias hartree = INTERNAL_ENERGY_UNIT')
-    ureg.define('@alias elementary_charge = INTERNAL_CHARGE_UNIT')
-    ureg.define('@alias K = INTERNAL_TEMPERATURE_UNIT')
+# Create ureg here so it is only created once
+from pint import UnitRegistry
+ureg = UnitRegistry()
+ureg.enable_contexts('spectroscopy')
+ureg.define('@alias bohr = INTERNAL_LENGTH_UNIT')
+ureg.define('@alias electron_mass = INTERNAL_MASS_UNIT')
+ureg.define('@alias hartree = INTERNAL_ENERGY_UNIT')
+ureg.define('@alias elementary_charge = INTERNAL_CHARGE_UNIT')
+ureg.define('@alias K = INTERNAL_TEMPERATURE_UNIT')
 
-    ureg.define('@alias angstrom = DEFAULT_LENGTH_UNIT')
-    ureg.define('@alias amu = DEFAULT_MASS_UNIT')
-    ureg.define('@alias eV = DEFAULT_ENERGY_UNIT')
-    ureg.define('@alias K = DEFAULT_TEMPERATURE_UNIT')
+ureg.define('@alias angstrom = DEFAULT_LENGTH_UNIT')
+ureg.define('@alias amu = DEFAULT_MASS_UNIT')
+ureg.define('@alias eV = DEFAULT_ENERGY_UNIT')
+ureg.define('@alias K = DEFAULT_TEMPERATURE_UNIT')
 
-    from .spectra import Spectrum1D, Spectrum2D
-    from .crystal import Crystal
-    from .debye_waller import DebyeWaller
-    from .structure_factor import StructureFactor
-    from .qpoint_phonon_modes import QpointPhononModes
-    from .force_constants import ForceConstants
-except ImportError:
-    pass
-
+from .spectra import Spectrum1D, Spectrum2D
+from .crystal import Crystal
+from .debye_waller import DebyeWaller
+from .structure_factor import StructureFactor
+from .qpoint_phonon_modes import QpointPhononModes
+from .force_constants import ForceConstants
