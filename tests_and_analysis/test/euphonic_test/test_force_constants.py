b472243dbd3624d0cc996d5d85f46d2c0a2026b9
JamesKingWork <james.c.king@stfc.ac.uk>
2020-05-29 14:06:42 +0100

    Other lzo test

--- tests_and_analysis/test/euphonic_test/test_force_constants.py
+++ tests_and_analysis/test/euphonic_test/test_force_constants.py
@@ -672,7 +672,7 @@ class TestInterpolatePhonons:
             for i in range(3):
                 self.unique_cell_origins[i] = [int(x)
                                                for x in f.readline().split()]
-        return ForceConstants.from_castep(filename), qpt,\
+        return ForceConstants.from_castep(filename), qpt, \
             expected_sc_phases, expected_cell_phases
 
     def test_calculate_phases(self, calculate_phases):
@@ -683,3 +683,65 @@ class TestInterpolatePhonons:
         )
         npt.assert_allclose(sc_phases, expected_sc_phases)
         npt.assert_allclose(cell_phases, expected_cell_phases)
+
+    # Supercell image calculation limit - 2 supercells in each direction
+    lim = 2
+
+    def get_ijn_from_image_data(self, image_data):
+        i = image_data[:, 0].astype(int)
+        j = image_data[:, 1].astype(int)
+        n = image_data[:, 2].astype(int)
+        return i, j, n
+
+    @pytest.fixture
+    def lzo_calculate_supercell_images_n_sc_images(self):
+        image_data = np.loadtxt(
+            os.path.join(self.path, 'LZO', 'lzo_n_sc_images.txt')
+        )
+        i, j, n = self.get_ijn_from_image_data(image_data)
+        expected_n_images = np.zeros((22, 88))
+        expected_n_images[i, j] = n
+        # After refactoring where the shape of n_sc_images was changed from
+        # (n_atoms, n_cells_in_sc*n_atoms) to (n_cells_in_sc, n_atoms, n_atoms),
+        # expected_n_images must be reshaped to ensure tests still pass
+        expected_n_images = np.transpose(np.reshape(
+            expected_n_images, (22, 4, 22)), axes=[1, 0, 2]
+        )
+        filename = os.path.join(self.path, 'LZO', 'La2Zr2O7.castep_bin')
+        return ForceConstants.from_castep(filename), "_n_sc_images", \
+            expected_n_images
+
+    @pytest.fixture
+    def lzo_calculate_supercell_images_sc_image_i(self):
+        image_data = np.loadtxt(
+            os.path.join(self.path, 'LZO', 'lzo_sc_image_i.txt')
+        )
+        i, j, n = self.get_ijn_from_image_data(image_data)
+        sc_i = image_data[:, 3].astype(int)
+        max_n = np.max(n) + 1
+        # size = n_atoms X n_atoms*n_cells_in_sc X max supercell images
+        expected_sc_image_i = np.full((22, 88, max_n), -1)
+        expected_sc_image_i[i, j, n] = sc_i
+        # After refactoring where the shape of sc_image_i was changed from
+        # (n_atoms, n_cells_in_sc*n_atoms, (2*lim + 1)**3) to
+        # (n_cells_in_sc, n_atoms, n_atoms, (2*lim + 1)**3),
+        # expctd_image_i must be reshaped to ensure tests still pass
+        expected_sc_image_i = np.transpose(
+            np.reshape(expected_sc_image_i, (22, 4, 22, max_n)),
+            axes=[1, 0, 2, 3]
+        )
+        filename = os.path.join(self.path, 'LZO', 'La2Zr2O7.castep_bin')
+        return ForceConstants.from_castep(filename), "_sc_image_i", \
+            expected_sc_image_i
+
+    @pytest.mark.parametrize('calculate_supercell_images', [
+        pytest.lazy_fixture('lzo_calculate_supercell_images_n_sc_images'),
+        pytest.lazy_fixture('lzo_calculate_supercell_images_sc_image_i')
+    ])
+    def test_calculate_supercell_images(self, calculate_supercell_images):
+        fc, testing_attribute, expected_attribute_data = \
+            calculate_supercell_images
+        fc._calculate_supercell_images(self.lim)
+        npt.assert_equal(
+            getattr(fc, testing_attribute), expected_attribute_data
+        )
