a63919b88396d061f0942c02aea3a6672e141f92
JamesKingWork <james.c.king@stfc.ac.uk>
2020-06-08 10:00:01 +0100

    Add nacl data back in

--- tests_and_analysis/test/euphonic_test/test_force_constants_calculate_qpt_phonons.py
+++ tests_and_analysis/test/euphonic_test/test_force_constants_calculate_qpt_phonons.py
@@ -96,6 +96,208 @@ lzo_and_graphite_test_data = [
     for kwargs_key in lzo_and_graphite_kwargs
 ]
 
+# Load quartz material test data
+quartz_qpts = np.array([
+    [0.00, 0.00, 0.00],
+    [0.00, 0.00, 0.50],
+    [-0.25, 0.50, 0.50],
+    [-0.151515, 0.575758, 0.5]
+])
+quartz_split_qpts = np.array([
+    [0.00, 0.00, 0.00],
+    [0.00, 0.00, 0.50],
+    [0.00, 0.00, 0.00],
+    [0.00, 0.00, 0.00],
+    [-0.25, 0.50, 0.50],
+    [0.00, 0.00, 0.00],
+    [0.00, 0.00, 0.00],
+    [0.00, 0.00, 0.00],
+    [0.00, 0.00, 0.00],
+    [0.00, 0.00, 0.00],
+    [0.00, 0.00, 0.00],
+    [-0.151515, 0.575758, 0.5],
+    [0.00, 0.00, 0.00]
+])
+quartz_split_qpts_insert_gamma = np.array([
+    [0.00, 0.00, 0.00],
+    [0.00, 0.00, 0.50],
+    [0.00, 0.00, 0.00],
+    [-0.25, 0.50, 0.50],
+    [0.00, 0.00, 0.00],
+    [0.00, 0.00, 0.00],
+    [0.00, 0.00, 0.00],
+    [-0.151515, 0.575758, 0.5],
+    [0.00, 0.00, 0.00]
+])
+
+quartz_kwargs_qpts_atol = [
+    {
+        "kwargs": {"dipole": True, "splitting": False},
+        "qpts": quartz_qpts,
+        "atol": 2e-6,
+        "key": "no_asr"
+    },
+    {
+        "kwargs": {
+            "dipole": True, "splitting": False,
+            "use_c": True, "fall_back_on_python": False
+        },
+        "qpts": quartz_qpts,
+        "atol": 2e-6,
+        "key": "no_asr"
+    },
+    {
+        "kwargs": {
+            "dipole": True, "splitting": False, "use_c": True,
+            "n_threads": 2, "fall_back_on_python": False
+        },
+        "qpts": quartz_qpts,
+        "atol": 8e-8,
+        "key": "no_asr"
+    },
+    {
+        "kwargs": {
+            "asr": 'reciprocal', "dipole": True, "splitting": False
+        },
+        "qpts": quartz_qpts,
+        "atol": 5e-4,
+        "key": "reciprocal"
+    },
+    {
+        "kwargs": {
+            "asr": 'reciprocal', "dipole": True,
+            "splitting": False, "use_c": True, "fall_back_on_python": False
+        },
+        "qpts": quartz_qpts,
+        "atol": 5e-4,
+        "key": "reciprocal"
+    },
+    {
+        "kwargs": {
+            "asr": 'reciprocal', "dipole": True,
+            "splitting": False, "use_c": True, "n_threads": 2,
+            "fall_back_on_python": False
+        },
+        "qpts": quartz_qpts,
+        "atol": 5e-4,
+        "key": "reciprocal"
+    },
+    {
+        "kwargs": {
+            "asr": 'reciprocal', "dipole": True, "splitting": True
+        },
+        "qpts": quartz_split_qpts,
+        "atol": 5e-4,
+        "key": "reciprocal_splitting"
+    },
+    {
+        "kwargs": {
+            "asr": 'reciprocal', "dipole": True,
+            "splitting": True, "use_c": True, "fall_back_on_python": False
+        },
+        "qpts": quartz_split_qpts,
+        "atol": 5e-4,
+        "key": "reciprocal_splitting"
+    },
+    {
+        "kwargs": {
+            "asr": 'reciprocal', "dipole": True,
+            "splitting": True, "use_c": True, "n_threads": 2,
+            "fall_back_on_python": False
+        },
+        "qpts": quartz_split_qpts,
+        "atol": 5e-4,
+        "key": "reciprocal_splitting"
+    },
+    {
+        "kwargs": {
+            "asr": 'reciprocal', "dipole": True,
+            "splitting": True, "insert_gamma": True
+        },
+        "qpts": quartz_split_qpts_insert_gamma,
+        "atol": 5e-4,
+        "key": "reciprocal_splitting"
+    }
+]
+
+quartz_test_data = [
+    {
+        "material": "quartz",
+        "json_file": os.path.join(
+            get_data_path(), "force_constants",
+            "quartz", "quartz_force_constants.json"
+        ),
+        **data_dict
+    } for data_dict in quartz_kwargs_qpts_atol
+]
+
+# Create test data for NaCl
+nacl_kwargs_key = [
+    {"kwargs": {"dipole": True, "splitting": False}, "key": "no_asr"},
+    {
+        "kwargs": {
+            "dipole": True, "splitting": False,
+            "use_c": True, "fall_back_on_python": False
+        }, "key": "no_asr"
+    },
+    {
+        "kwargs": {
+            "dipole": True, "splitting": False,
+            "use_c": True, "fall_back_on_python": False, "n_threads": 2
+        }, "key": "no_asr"
+    },
+    {
+        "kwargs": {"dipole": True, "splitting": False, "asr": 'reciprocal'},
+        "key": "reciprocal"
+    },
+    {
+        "kwargs": {
+            "dipole": True, "splitting": False, "asr": 'reciprocal',
+            "use_c": True, "fall_back_on_python": False
+        }, "key": "reciprocal"
+    },
+    {
+        "kwargs": {
+            "dipole": True, "splitting": False, "asr": 'reciprocal',
+            "use_c": True, "fall_back_on_python": False, "n_threads": 2
+        }, "key": "reciprocal"
+    },
+    {
+        "kwargs":  {"dipole": True, "splitting": False, "asr": 'realspace'},
+        "key": "realspace"
+    },
+    {
+        "kwargs": {
+            "dipole": True, "splitting": False, "asr": 'realspace',
+            "use_c": True, "fall_back_on_python": False
+        }, "key": "realspace"
+    },
+    {
+        "kwargs": {
+            "dipole": True, "splitting": False, "asr": 'realspace',
+            "use_c": True, "fall_back_on_python": False, "n_threads": 2
+        }, "key": "realspace"
+    }
+]
+
+nacl_test_data = [
+    {
+        "material": "NaCl",
+        "json_file": os.path.join(
+            get_data_path(), "phonopy_data", "NaCl",
+            "force_constants", "phonopy-yaml.json"
+        ),
+        "atol": 1e-8,
+        "qpts": np.array([
+            [0.00, 0.00, 0.00],
+            [0.00, 0.00, 0.50],
+            [-0.25, 0.50, 0.50],
+            [-0.151515, 0.575758, 0.5]
+        ]),
+        **data_dict
+    } for data_dict in nacl_kwargs_key
+]
+
 
 @pytest.mark.integration
 class TestCalculateQPointPhononModes:
@@ -104,144 +306,8 @@ class TestCalculateQPointPhononModes:
         os.path.join(get_data_path(), "force_constants", "expected_freqs.json")
     )
 
-    # Load quartz material test data
-
-    quartz_qpts = np.array([
-        [0.00, 0.00, 0.00],
-        [0.00, 0.00, 0.50],
-        [-0.25, 0.50, 0.50],
-        [-0.151515, 0.575758, 0.5]
-    ])
-    quartz_split_qpts = np.array([
-        [0.00, 0.00, 0.00],
-        [0.00, 0.00, 0.50],
-        [0.00, 0.00, 0.00],
-        [0.00, 0.00, 0.00],
-        [-0.25, 0.50, 0.50],
-        [0.00, 0.00, 0.00],
-        [0.00, 0.00, 0.00],
-        [0.00, 0.00, 0.00],
-        [0.00, 0.00, 0.00],
-        [0.00, 0.00, 0.00],
-        [0.00, 0.00, 0.00],
-        [-0.151515, 0.575758, 0.5],
-        [0.00, 0.00, 0.00]
-    ])
-    quartz_split_qpts_insert_gamma = np.array([
-        [0.00, 0.00, 0.00],
-        [0.00, 0.00, 0.50],
-        [0.00, 0.00, 0.00],
-        [-0.25, 0.50, 0.50],
-        [0.00, 0.00, 0.00],
-        [0.00, 0.00, 0.00],
-        [0.00, 0.00, 0.00],
-        [-0.151515, 0.575758, 0.5],
-        [0.00, 0.00, 0.00]
-    ])
-
-    quartz_kwargs_qpts_atol = [
-        {
-            "kwargs": {"dipole": True, "splitting": False},
-            "qpts": quartz_qpts,
-            "atol": 2e-6,
-            "key": "no_asr"
-        },
-        {
-            "kwargs": {
-                "dipole": True, "splitting": False,
-                "use_c": True, "fall_back_on_python": False
-            },
-            "qpts": quartz_qpts,
-            "atol": 2e-6,
-            "key": "no_asr"
-        },
-        {
-            "kwargs": {
-                "dipole": True, "splitting": False, "use_c": True,
-                "n_threads": 2, "fall_back_on_python": False
-            },
-            "qpts": quartz_qpts,
-            "atol": 8e-8,
-            "key": "no_asr"
-        },
-        {
-            "kwargs": {
-                "asr": 'reciprocal', "dipole": True, "splitting": False
-            },
-            "qpts": quartz_qpts,
-            "atol": 5e-4,
-            "key": "reciprocal"
-        },
-        {
-            "kwargs": {
-                "asr": 'reciprocal', "dipole": True,
-                "splitting": False, "use_c": True, "fall_back_on_python": False
-            },
-            "qpts": quartz_qpts,
-            "atol": 5e-4,
-            "key": "reciprocal"
-        },
-        {
-            "kwargs": {
-                "asr": 'reciprocal', "dipole": True,
-                "splitting": False, "use_c": True, "n_threads": 2,
-                "fall_back_on_python": False
-            },
-            "qpts": quartz_qpts,
-            "atol": 5e-4,
-            "key": "reciprocal"
-        },
-        {
-            "kwargs": {
-                "asr": 'reciprocal', "dipole": True, "splitting": True
-            },
-            "qpts": quartz_split_qpts,
-            "atol": 5e-4,
-            "key": "reciprocal_splitting"
-        },
-        {
-            "kwargs": {
-                "asr": 'reciprocal', "dipole": True,
-                "splitting": True, "use_c": True, "fall_back_on_python": False
-            },
-            "qpts": quartz_split_qpts,
-            "atol": 5e-4,
-            "key": "reciprocal_splitting"
-        },
-        {
-            "kwargs": {
-                "asr": 'reciprocal', "dipole": True,
-                "splitting": True, "use_c": True, "n_threads": 2,
-                "fall_back_on_python": False
-            },
-            "qpts": quartz_split_qpts,
-            "atol": 5e-4,
-            "key": "reciprocal_splitting"
-        },
-        {
-            "kwargs": {
-                "asr": 'reciprocal', "dipole": True,
-                "splitting": True, "insert_gamma": True
-            },
-            "qpts": quartz_split_qpts_insert_gamma,
-            "atol": 5e-4,
-            "key": "reciprocal_splitting"
-        }
-    ]
-
-    quartz_test_data = [
-        {
-            "material": "quartz",
-            "json_file": os.path.join(
-                get_data_path(), "force_constants",
-                "quartz", "quartz_force_constants.json"
-            ),
-            **data_dict
-        } for data_dict in quartz_kwargs_qpts_atol
-    ]
-
     @pytest.fixture(
-        params=lzo_and_graphite_test_data + quartz_test_data
+        params=lzo_and_graphite_test_data + quartz_test_data + nacl_test_data
     )
     def create_fc_and_formulate_args(self, request):
         test_data = request.param
@@ -251,57 +317,6 @@ class TestCalculateQPointPhononModes:
         fc = ForceConstants.from_json_file(filename)
         return fc, kwargs, test_data["material"], \
             test_data["key"], test_data["atol"]
-
-    # nacl_yaml_dir = os.path.join(
-    #     get_data_path(), "phonopy_data", "NaCl", "force_constants"
-    # )
-    #
-    # @pytest.fixture(params=[
-    #         {"dipole": True, "splitting": False},
-    #         {
-    #             "dipole": True, "splitting": False,
-    #             "use_c": True, "fall_back_on_python": False
-    #         },
-    #         {
-    #             "dipole": True, "splitting": False,
-    #             "use_c": True, "fall_back_on_python": False, "n_threads": 2
-    #         },
-    #         {"dipole": True, "splitting": False, "asr": 'reciprocal'},
-    #         {
-    #             "dipole": True, "splitting": False, "asr": 'reciprocal',
-    #             "use_c": True, "fall_back_on_python": False
-    #         },
-    #         {
-    #             "dipole": True, "splitting": False, "asr": 'reciprocal',
-    #             "use_c": True, "fall_back_on_python": False, "n_threads": 2
-    #         },
-    #         {"dipole": True, "splitting": False, "asr": 'realspace'},
-    #         {
-    #             "dipole": True, "splitting": False, "asr": 'realspace',
-    #             "use_c": True, "fall_back_on_python": False
-    #         },
-    #         {
-    #             "dipole": True, "splitting": False, "asr": 'realspace',
-    #             "use_c": True, "fall_back_on_python": False, "n_threads": 2
-    #         }
-    #     ]
-    # )
-    # def create_from_nacl(self, request):
-    #     fc = ForceConstants.from_phonopy(
-    #         path=self.nacl_yaml_dir, summary_name="phonopy.yaml"
-    #     )
-    #     kwargs = request.param
-    #     key = kwargs["asr"] if "asr" in kwargs else "no_asr"
-    #     qpts = np.array([
-    #         [0.00, 0.00, 0.00],
-    #         [0.00, 0.00, 0.50],
-    #         [-0.25, 0.50, 0.50],
-    #         [-0.151515, 0.575758, 0.5]
-    #     ])
-    #     kwargs["qpts"] = qpts
-    #     atol = 1e-8
-    #     material_name = "NaCl"
-    #     return fc, kwargs, material_name, key, atol
 
     def test_fc_calculate_qpoint_phonon_modes_expected_results(
             self, create_fc_and_formulate_args):
