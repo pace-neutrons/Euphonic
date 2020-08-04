d6640cf4572abe6278ef2e9599288745beb6e463
Adam J. Jackson <a.j.jackson@physics.org>
2020-08-04 16:58:39 +0100

    Clean up neutron band structure script

--- scripts/neutron-band-structure.py
+++ scripts/neutron-band-structure.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 import argparse
 import os
+from typing import List, Tuple
 
 import euphonic
 from euphonic import ureg
@@ -25,6 +26,77 @@ def get_parser() -> argparse.ArgumentParser:
     return parser
 
 
+def get_seekpath_structure(crystal: euphonic.Crystal) -> Tuple[np.ndarray,
+                                                               np.ndarray,
+                                                               List[int]]:
+    # Seekpath needs a set of integer identities, while Crystal stores strings
+    # so we need to convert e.g. ['H', 'C', 'Cl', 'C'] -> [0, 1, 2, 1]
+    all_atom_types = list(set(crystal.atom_type.tolist()))
+    atom_id_list = [all_atom_types.index(symbol)
+                    for symbol in crystal.atom_type]
+
+    return (crystal.cell_vectors.to('angstrom').magnitude,
+            crystal.atom_r,
+            atom_id_list)
+
+
+def _get_break_points(bandpath: dict) -> Tuple[List[Tuple[int, int]],
+                                               List[int]]:
+    """Get information about band path labels and break points
+
+    Parameters
+    ----------
+    bandpath
+        Bandpath dictionary from Seekpath
+
+    Returns
+    -------
+    (break_points, special_point_indices)
+
+    ``break_points`` is a list of tuples identifying the boundaries of regions
+    with discontinuities between them, e.g.::
+
+      [(0, end1), (start2, end2), (start3, end3)]
+
+    for a band path with three segments. end3 should equal the index of the
+    last k-point in the full path.
+
+    ``special_point_indices`` is a list of locations for labelled points in the
+    full set of labels from Seekpath.
+
+    """
+    # Find break points between continuous spectra: wherever there are two
+    # adjacent labels
+    special_point_bools = np.fromiter(
+        map(bool, bandpath["explicit_kpoints_labels"]), dtype=bool)
+    special_point_indices = special_point_bools.nonzero()[0]
+
+    # [T F F T T F T] -> [F F T T F T] AND [T F F T T F] = [F F F T F F] -> 3,
+    break_points = (np.logical_and(special_point_bools[:-1],
+                                   special_point_bools[1:])
+                    .nonzero()[0].tolist())
+
+    return break_points, special_point_indices
+
+
+def _get_tick_labels(region: Tuple[int, int],
+                     bandpath: dict,
+                     special_point_indices) -> List[Tuple[int, str]]:
+    start, end = region
+    label_indices = special_point_indices[
+        np.logical_and(special_point_indices >= start,
+                       special_point_indices < end)]
+
+    labels = [bandpath["explicit_kpoints_labels"][i]
+              for i in label_indices]
+    for i, label in enumerate(labels):
+        if label == 'GAMMA':
+            labels[i] = r'$\Gamma$'
+
+    label_indices_shifted = [i - start for i in label_indices]
+    return list(zip(label_indices_shifted, labels))
+
+
 def main():
     args = get_parser().parse_args()
     filename = args.file
@@ -36,30 +108,11 @@ def main():
         path=path, summary_name=summary_name)
 
     print(f"Getting band path")
-    # Seekpath needs a set of integer identities, while Crystal stores strings
-    # so we need to convert e.g. ['H', 'C', 'Cl', 'C'] -> [0, 1, 2, 1]
-    all_atom_types = list(set(force_constants.crystal.atom_type.tolist()))
-    atom_id_list = [all_atom_types.index(symbol)
-                    for symbol in force_constants.crystal.atom_type]
-
-    structure = (force_constants.crystal.cell_vectors.to('angstrom').magnitude,
-                 force_constants.crystal.atom_r,
-                 atom_id_list)
-
+    structure = get_seekpath_structure(force_constants.crystal)
     bandpath = seekpath.get_explicit_k_path(structure,
                                             reference_distance=args.q_distance)
 
-    # Find break points between continuous spectra: wherever there are two
-    # adjacent labels
-    special_point_bools = np.fromiter(
-        map(bool, bandpath["explicit_kpoints_labels"]), dtype=bool)
-    # End points are always special even if unlabeled
-    special_point_bools[0] = special_point_bools[-1] = True
-
-    # [T F F T T F T] -> [F F T T F T] AND [T F F T T F] = [F F F T F F] -> 3,
-    break_points = (np.logical_and(special_point_bools[:-1],
-                                   special_point_bools[1:])
-                    .nonzero()[0].tolist())
+    break_points, special_point_indices = _get_break_points(bandpath)
     if break_points:
         print(f"Found {len(break_points)} regions in q-point path")
 
@@ -68,7 +121,7 @@ def main():
     for break_point in break_points:
         regions.append((start_point, break_point + 1))
         start_point = break_point + 1
-    regions.append((start_point, len(special_point_bools)))
+    regions.append((start_point, len(bandpath["explicit_kpoints_rel"])))
 
     print("Computing phonon modes")
     region_modes = []
@@ -86,30 +139,15 @@ def main():
     ebins = np.linspace(emin.to('meV').magnitude, emax.to('meV').magnitude,
                         args.ebins) * ureg['meV']
 
-    print(f"Computing structure factor")
-    structure_factors = []
-    for modes in region_modes:
-        structure_factors.append(modes.calculate_structure_factor())
+    print("Computing structure factors and generating 2D maps")
+    for region, modes in zip(regions, region_modes):
 
-    print(f"Compiling structure factor to 2D map")
-    special_point_indices = special_point_bools.nonzero()[0]
-
-    for (region_start, region_end), structure_factor in zip(regions,
-                                                            structure_factors):
+        structure_factor = modes.calculate_structure_factor()
         sqw = structure_factor.calculate_sqw_map(ebins)
+
         if args.seekpath_labels:
-            label_indices = special_point_indices[
-                np.logical_and(special_point_indices >= region_start,
-                               special_point_indices < region_end)]
-
-            labels = [bandpath["explicit_kpoints_labels"][i]
-                      for i in label_indices]
-            for i, label in enumerate(labels):
-                if label == 'GAMMA':
-                    labels[i] = r'$\Gamma$'
-
-            label_indices_shifted = [i - region_start for i in label_indices]
-            sqw.x_tick_labels = list(zip(label_indices_shifted, labels))
+            sqw.x_tick_labels = _get_tick_labels(region, bandpath,
+                                                 special_point_indices)
 
         print(f"Plotting figure")
         euphonic.plot.plot_2d(sqw)
