693aba65e699bcf33614e526bb16dd07883877c7
Adam J. Jackson <a.j.jackson@physics.org>
2020-08-11 13:44:11 +0100

    Spherical sampling: more tests

--- tests_and_analysis/test/euphonic_test/test_sampling.py
+++ tests_and_analysis/test/euphonic_test/test_sampling.py
@@ -4,8 +4,8 @@ from typing import Iterator
 
 import euphonic.sampling
 
-class TestSquareSampling:
 
+class TestSquareSampling:
     gold_square_ref_data = [({'npts': 3, 'offset': False, 'jitter': False},
                              [[0, 0],
                               [0.3333333333333333, 0.61803398874989480],
@@ -24,5 +24,68 @@ class TestSquareSampling:
     def test_golden_square(self, params, ref_results):
         np.random.seed(0)
         points_iter = euphonic.sampling.golden_square(**params)
+        assert isinstance(points_iter, Iterator)
+        assert np.allclose(list(points_iter), ref_results)
+
+    reg_square_ref_data = [({'n_rows': 2, 'n_cols': 4,
+                             'offset': False, 'jitter': False},
+                            [[0., 0.], [0., 0.5],
+                             [0.25, 0.], [0.25, 0.5],
+                             [0.5, 0.], [0.5, 0.5],
+                             [0.75, 0.], [0.75, 0.5]]),
+                           ({'n_rows': 2, 'n_cols': 4,
+                             'offset': True, 'jitter': False},
+                            [[0.125, 0.25], [0.125, 0.75],
+                             [0.375, 0.25], [0.375, 0.75],
+                             [0.625, 0.25], [0.625, 0.75],
+                             [0.875, 0.25], [0.875, 0.75]]),
+                           ({'n_rows': 1, 'n_cols': 2,
+                             'offset': False, 'jitter': True},
+                            [[0.024406751963662376, 0.21518936637241948],
+                             [0.551381688035822, 0.044883182996896864]])
+                           ]
+
+    @pytest.mark.parametrize('params, ref_results', reg_square_ref_data)
+    def test_regular_square(self, params, ref_results):
+        np.random.seed(0)
+        points_iter = euphonic.sampling.regular_square(**params)
+        assert isinstance(points_iter, Iterator)
+        assert np.allclose(list(points_iter), ref_results)
+
+class TestSphereSampling:
+
+    gold_sphere_ref = [({'npts': 4,
+                         'cartesian': False, 'jitter': False},
+                        [[1, 0.0, 2.4188584057763776],
+                         [1, 3.8832220774509327, 1.8234765819369754],
+                         [1, 1.483258847722279, 1.318116071652818],
+                         [1, 5.366480925173213, 0.7227342478134157]]),
+                       ({'npts': 4,
+                         'cartesian': True, 'jitter': False},
+                        [[0.6614378277661477, 0.0, -0.75],
+                         [-0.7139543462022453, -0.6540406650499068, -0.25],
+                         [0.08464959396472624, 0.9645384628108965, 0.25],
+                         [0.4024444785343673, -0.5249175570479627, 0.75]])]
+
+    @pytest.mark.parametrize('params, ref_results', gold_sphere_ref)
+    def test_golden_sphere(self, params, ref_results):
+        points_iter = euphonic.sampling.golden_sphere(**params)
+        assert isinstance(points_iter, Iterator)
+        assert np.allclose(list(points_iter), ref_results)
+
+    square_sphere_ref = [({'n_rows': 1, 'n_cols': 3,
+                           'cartesian': False, 'jitter': False},
+                          [[1.        , 3.14159265, 2.30052398],
+                           [1.        , 3.14159265, 1.57079633],
+                           [1.        , 3.14159265, 0.84106867]]),
+                         ({'n_rows': 3, 'n_cols': 1,
+                           'cartesian': True, 'jitter': False},
+                          [(0.5, 0.8660254037844386, 0.),
+                           (-1., 0., 0.),
+                           (0.5, -0.866025403784439, 0.)])]
+
+    @pytest.mark.parametrize('params, ref_results', square_sphere_ref)
+    def test_sphere_from_square_grid(self, params, ref_results):
+        points_iter = euphonic.sampling.sphere_from_square_grid(**params)
         assert isinstance(points_iter, Iterator)
         assert np.allclose(list(points_iter), ref_results)
