1f5685eca54b95fc96768d9384de20f135f1bf68
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2020-12-08 17:48:48 +0000

    Refactor script tests
     - Put script arguments and output files in test file rather than utils
     - Enable regeneration of test data files via Pytest (as a test with
       skip turned on by default)

--- tests_and_analysis/test/script_tests/test_dispersion.py
+++ tests_and_analysis/test/script_tests/test_dispersion.py
@@ -1,13 +1,25 @@
 import sys
+import os
 import json
+from unittest.mock import patch
+
 import pytest
 import numpy as np
 import numpy.testing as npt
 # Required for mocking
 import matplotlib.pyplot
-from .utils import (get_phonon_file, get_dispersion_params,
-                    get_dispersion_data_file)
+
+from tests_and_analysis.test.utils import get_data_path
+from tests_and_analysis.test.script_tests.utils import (
+    get_script_test_data_path, get_current_plot_lines_xydata)
 import euphonic.cli.dispersion
+
+
+nah_phonon_file = os.path.join(
+    get_data_path(), 'qpoint_phonon_modes', 'NaH', 'NaH.phonon')
+disp_output_file = os.path.join(get_script_test_data_path(), "dispersion.json")
+disp_params =  [[], ["--energy-unit=meV"], ["--btol=5.0"], ["--reorder"],
+                ["--asr"], ["--asr=realspace"]]
 
 
 @pytest.mark.integration
@@ -24,17 +36,29 @@ class TestRegression:
         # Ensure figures are closed
         matplotlib.pyplot.close('all')
 
-    @pytest.mark.parametrize("dispersion_args", get_dispersion_params())
+    @pytest.mark.parametrize("dispersion_args", disp_params)
     def test_plots_produce_expected_xydata(
             self, inject_mocks, dispersion_args):
-        euphonic.cli.dispersion.main([get_phonon_file()] + dispersion_args)
+        euphonic.cli.dispersion.main([nah_phonon_file] + dispersion_args)
 
         lines = matplotlib.pyplot.gcf().axes[0].lines
 
-        with open(get_dispersion_data_file()) as disp_json_file:
-            expected_lines = json.load(
-                disp_json_file)[" ".join(dispersion_args)]
+        with open(disp_output_file, 'r') as f:
+            expected_lines = json.load(f)[" ".join(dispersion_args)]
         for index, line in enumerate(lines):
             npt.assert_allclose(
                 line.get_xydata().T, np.array(expected_lines[index]),
                 atol=sys.float_info.epsilon)
+
+
+@patch("matplotlib.pyplot.show")
+@pytest.mark.skip(reason='Only run if you want to regenerate the test data')
+def test_regenerate_disp_data(_):
+    json_data = {}
+    for disp_param in disp_params:
+        # Generate current figure for us to retrieve with gcf
+        euphonic.cli.dispersion.main([nah_phonon_file] + disp_param)
+        # Retrieve with gcf and record data
+        json_data[" ".join(disp_param)] = get_current_plot_lines_xydata()
+    with open(disp_output_file, "w+") as json_file:
+        json.dump(json_data, json_file, indent=4)
--- tests_and_analysis/test/script_tests/test_dos.py
+++ tests_and_analysis/test/script_tests/test_dos.py
@@ -1,12 +1,26 @@
 import json
+import os
 import sys
+from unittest.mock import patch
+
 import pytest
 import numpy as np
 import numpy.testing as npt
 # Required for mocking
 import matplotlib.pyplot
-from .utils import get_phonon_file, get_dos_params, get_dos_data_file
+
+from tests_and_analysis.test.utils import get_data_path
+from tests_and_analysis.test.script_tests.utils import (
+    get_script_test_data_path, get_current_plot_lines_xydata)
 import euphonic.cli.dos
+
+
+nah_phonon_file = os.path.join(
+    get_data_path(), 'qpoint_phonon_modes', 'NaH', 'NaH.phonon')
+dos_output_file = os.path.join(get_script_test_data_path(), "dos.json")
+dos_params = [[], ["--energy-broadening=2.3"], ["--ebins=100"],
+              ["--energy-broadening=2.3", "--ebins=100"],
+              ["--energy-unit=meV"], ["--shape=lorentz"]]
 
 
 @pytest.mark.integration
@@ -23,16 +37,29 @@ class TestRegression:
         # Ensure figures are closed
         matplotlib.pyplot.close('all')
 
-    @pytest.mark.parametrize("dos_args", get_dos_params())
+    @pytest.mark.parametrize("dos_args", dos_params)
     def test_plots_produce_expected_xydata(self, inject_mocks, dos_args):
-        euphonic.cli.dos.main([get_phonon_file()] + dos_args)
+        euphonic.cli.dos.main([nah_phonon_file] + dos_args)
 
         lines = matplotlib.pyplot.gcf().axes[0].lines
 
-        with open(get_dos_data_file()) as dos_json_file:
-            expected_lines = json.load(dos_json_file)[" ".join(dos_args)]
+        with open(dos_output_file, 'r') as f:
+            expected_lines = json.load(f)[" ".join(dos_args)]
 
         for index, line in enumerate(lines):
             npt.assert_allclose(line.get_xydata().T,
                                 np.array(expected_lines[index]),
                                 atol=5*sys.float_info.epsilon)
+
+
+@patch("matplotlib.pyplot.show")
+@pytest.mark.skip(reason='Only run if you want to regenerate the test data')
+def test_regenerate_dos_data(_):
+    json_data = {}
+    for dos_param in dos_params:
+        # Generate current figure for us to retrieve with gcf
+        euphonic.cli.dos.main([nah_phonon_file] + dos_param)
+        # Retrieve with gcf and record data
+        json_data[" ".join(dos_param)] = get_current_plot_lines_xydata()
+    with open(dos_output_file, "w+") as json_file:
+        json.dump(json_data, json_file, indent=4)
