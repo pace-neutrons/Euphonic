7d74074c3109e998e32d44a8407ae26dc2408cc6
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2020-12-08 12:16:36 +0000

    Move common args to cli utils

--- euphonic/cli/dispersion.py
+++ euphonic/cli/dispersion.py
@@ -10,7 +10,7 @@ from typing import List
 import euphonic
 from euphonic.plot import plot_1d
 from .utils import (load_data_from_file, get_args, _bands_from_force_constants,
-                    _get_q_distance, matplotlib_save_or_show)
+                    _get_q_distance, matplotlib_save_or_show, _get_cli_parser)
 
 
 def main(params: List[str] = None):
@@ -38,12 +38,20 @@ def main(params: List[str] = None):
         modes.reorder_frequencies()
 
     spectrum = modes.get_dispersion()
-    if x_tick_labels is not None:
+
+    if args.y_label is None:
+        y_label = f"Energy / {spectrum.y_data.units:~P}"
+    else:
+        y_label = args.y_label
+
+    if x_tick_labels:
         spectrum.x_tick_labels = x_tick_labels
+
     spectra = spectrum.split(**split_args)
 
     _ = plot_1d(spectra,
                 title=args.title,
+                x_label=args.x_label,
                 y_label=f'Energy ({spectrum.y_data.units:~P})',
                 y_min=args.e_min, y_max=args.e_max,
                 lw=1.0)
@@ -51,62 +59,14 @@ def main(params: List[str] = None):
 
 
 def get_parser():
-    parser = argparse.ArgumentParser(
-        description=('Extract band structure data from a .phonon file '
-                     'and plot it with matplotlib'))
-    parser.add_argument(
-        'filename',
-        help=('Phonon data file. This should contain force constants or band '
-              'data. Force constants formats: .yaml, force_constants.hdf5 '
-              '(Phonopy); .castep_bin , .check (Castep); .json (Euphonic). [A '
-              'band structure path will be obtained using Seekpath.  ] Band '
-              'data formats: {band,qpoints,mesh}.{hdf5,yaml} (Phonopy); '
-              '.phonon (Castep); .json (Euphonic)'
-              ))
-    parser.add_argument(
-        '-s', '--save-to', dest='save_to', default=None,
-        help='Save resulting plot to a file with this name')
-    parser.add_argument(
-        '-u', '--energy-unit', dest='energy_unit', default='meV',
-        help=('Convert frequencies to specified units for plotting (e.g 1/cm'))
-    parser.add_argument('--length-unit', type=str, default='angstrom',
-                        dest='length_unit',
-                        help=('Length units; these will be inverted to obtain '
-                              'units of distance between q-points (e.g. "bohr"'
-                              ' for bohr^-1).'))
-    parser.add_argument('--e-min', type=float, default=None, dest='e_min',
-                        help='Energy range minimum in ENERGY_UNIT')
-    parser.add_argument('--e-max', type=float, default=None, dest='e_max',
-                        help='Energy range maximum in ENERGY_UNIT')
-    parser.add_argument('--title', type=str, default='', help='Plot title')
+    parser = _get_cli_parser(qe_plot=True)
+    parser.description = (
+        'Plots a band structure from the file provided. If a force '
+        'constants file is provided, a band structure path is '
+        'generated using Seekpath')
     parser.add_argument(
         '--reorder',
         action='store_true',
         help=('Try to determine branch crossings from eigenvectors and'
               ' rearrange frequencies accordingly'))
-    disp_group = parser.add_argument_group(
-        'Dispersion arguments',
-        'Arguments specific to plotting a pre-calculated band structure')
-    disp_group.add_argument(
-        '--btol',
-        default=10.0,
-        type=float,
-        help=('The tolerance for plotting sections of reciprocal space on'
-              ' different subplots, as a fraction of the median distance'
-              ' between q-points'))
-
-    interp_group = parser.add_argument_group(
-        'Interpolation arguments',
-        ('Arguments specific to band structures that are generated from '
-         'Force Constants data'))
-    interp_group.add_argument(
-        '--asr', type=str, nargs='?',
-        default=None, const='reciprocal', choices=('reciprocal', 'realspace'),
-        help=('Apply an acoustic-sum-rule (ASR) correction to the data: '
-              '"realspace" applies the correction to the force constant '
-              'matrix in real space. "reciprocal" applies the correction to '
-              'the dynamical matrix at each q-point.'))
-    interp_group.add_argument(
-        '--q-distance', type=float, dest='q_distance', default=0.025,
-        help=('Target distance between q-point samples in 1/LENGTH_UNIT'))
     return parser
