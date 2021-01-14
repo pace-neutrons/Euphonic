8ee6e7233cde467f02ca4b97ecb41cf981f0cf42
Adam J. Jackson <a.j.jackson@physics.org>
2021-01-14 19:07:42 +0000

    Plotting module tests: _plot_1d_core from Spectrum1DCollection

--- tests_and_analysis/test/euphonic_test/test_plot.py
+++ tests_and_analysis/test/euphonic_test/test_plot.py
@@ -16,28 +16,31 @@ def figure():
     yield fig
     plt.close(fig)
 
+
 @pytest.fixture
 def axes(figure):
     ax = figure.add_subplot()
     return ax
 
+
 class TestPlot1DCore:
     @pytest.mark.parametrize('spectra, expected_error',
-                             [('wrong_type', TypeError),])
+                             [('wrong_type', TypeError), ])
     def test_1d_core_errors(self, spectra, expected_error, axes):
         with pytest.raises(expected_error):
             _plot_1d_core(spectra, axes)
 
-    @pytest.mark.parametrize('spectrum_params, expected_data',
-                        [# Case 1: Trivial
-                         ((np.array([0., 1., 2.]) * ureg('meV'),
-                           np.array([2., 3., 2.]) * ureg('angstrom^-2')),
-                          ([[0., 1., 2.], [2., 3., 2]],)),
-                         # Case 2: Split points create new line
-                         ((np.array([0., 1., 1., 2.]) * ureg('meV'),
-                           np.array([2., 3., 2., 4.]) * ureg('angstrom^-2')),
-                          ([[0., 1.], [2., 3.]], [[1., 2.], [2., 4.]]))
-                          ])
+    @pytest.mark.parametrize(
+        'spectrum_params, expected_data',
+        [  # Case 1: Trivial
+         ((np.array([0., 1., 2.]) * ureg('meV'),
+           np.array([2., 3., 2.]) * ureg('angstrom^-2')),
+          ([[0., 1., 2.], [2., 3., 2]],)),
+         # Case 2: Split points create new line
+         ((np.array([0., 1., 1., 2.]) * ureg('meV'),
+           np.array([2., 3., 2., 4.]) * ureg('angstrom^-2')),
+          ([[0., 1.], [2., 3.]], [[1., 2.], [2., 4.]]))
+          ])
     def test_plot_single_spectrum(self, spectrum_params, expected_data, axes):
         _plot_1d_core(Spectrum1D(*spectrum_params), axes)
 
@@ -47,11 +50,25 @@ class TestPlot1DCore:
             npt.assert_allclose(line.get_xdata(), expected[0])
             npt.assert_allclose(line.get_ydata(), expected[1])
 
-    # def test_plot_collection(self, simple_spectrum1d, axes):
-    #     _plot_1d_core(simple_spectrum1d, axes)
+    @pytest.mark.parametrize(
+        'spectrum_params, expected_data',
+        [  # Case 1: Two lines
+         ((np.array([0., 1., 2.]) * ureg('meV'),
+           np.array([[2., 3., 2.],
+                     [3., 4., 3.]]) * ureg('angstrom^-2')),
+          ([[0., 1., 2.], [2., 3., 2.]],
+           [[0., 1., 2.], [3., 4., 3.]])),
+         # Case 2: Two lines with split points
+         ((np.array([0., 1., 1., 2.]) * ureg('meV'),
+           np.array([[2., 3., 2., 4.],
+                     [5., 4., 3., 2.]]) * ureg('angstrom^-2')),
+          ([[0., 1.], [2., 3.]], [[1., 2.], [2., 4.]],
+           [[0., 1.], [5., 4.]], [[1., 2.], [3., 2.]]))
+           ])
+    def test_plot_collection(self, spectrum_params, expected_data, axes):
+        _plot_1d_core(Spectrum1DCollection(*spectrum_params), axes)
+        assert len(expected_data) == len(axes.lines)
+        for line, expected in zip(axes.lines, expected_data):
 
-    #     assert len(axes.lines) == 1
-    #     npt.assert_allclose(axes.lines[0].get_xdata(),
-    #                         simple_spectrum1d.x_data.magnitude)
-    #     npt.assert_allclose(axes.lines[0].get_ydata(),
-    #                         simple_spectrum1d.y_data.magnitude)
+            npt.assert_allclose(line.get_xdata(), expected[0])
+            npt.assert_allclose(line.get_ydata(), expected[1])
