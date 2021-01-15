47e5144219cfb65b854d88860fd0576ec25378a1
Adam J. Jackson <a.j.jackson@physics.org>
2021-01-15 19:26:27 +0000

    Plot tests: 1D tick labels

--- tests_and_analysis/test/euphonic_test/test_plot.py
+++ tests_and_analysis/test/euphonic_test/test_plot.py
@@ -31,24 +31,35 @@ class TestPlot1DCore:
             _plot_1d_core(spectra, axes)
 
     @pytest.mark.parametrize(
-        'spectrum_params, expected_data',
+        'spectrum_params, spectrum_kwargs, expected_data, expected_ticks',
         [  # Case 1: Trivial
          ((np.array([0., 1., 2.]) * ureg('meV'),
            np.array([2., 3., 2.]) * ureg('angstrom^-2')),
-          ([[0., 1., 2.], [2., 3., 2]],)),
+          {'x_tick_labels': [(0, 'A'), (2, 'B')]},
+          ([[0., 1., 2.], [2., 3., 2]],),
+          [(0., 'A'), (2., 'B')]),
          # Case 2: Split points create new line
          ((np.array([0., 1., 1., 2.]) * ureg('meV'),
            np.array([2., 3., 2., 4.]) * ureg('angstrom^-2')),
-          ([[0., 1.], [2., 3.]], [[1., 2.], [2., 4.]]))
-          ])
-    def test_plot_single_spectrum(self, spectrum_params, expected_data, axes):
-        _plot_1d_core(Spectrum1D(*spectrum_params), axes)
+          {'x_tick_labels': [(1, 'B'), (2, 'B'), (3, 'C')]},
+          ([[0., 1.], [2., 3.]], [[1., 2.], [2., 4.]]),
+          #  Note that duplicated points get plotted twice. Weird but harmless.
+          [(1., 'B'), (1., 'B'), (2., 'C')]
+          )])
+    def test_plot_single_spectrum(self, spectrum_params, spectrum_kwargs,
+                                  expected_data, expected_ticks, axes):
+        _plot_1d_core(Spectrum1D(*spectrum_params, **spectrum_kwargs), axes)
 
         assert len(expected_data) == len(axes.lines)
         for line, expected in zip(axes.lines, expected_data):
 
             npt.assert_allclose(line.get_xdata(), expected[0])
             npt.assert_allclose(line.get_ydata(), expected[1])
+
+        tick_locs, tick_labels = zip(*expected_ticks)
+        npt.assert_allclose(axes.get_xticks(), tick_locs)
+        assert [label.get_text() for label in axes.get_xticklabels()
+                ] == list(tick_labels)
 
     @pytest.mark.parametrize(
         'spectrum_params, expected_data',
