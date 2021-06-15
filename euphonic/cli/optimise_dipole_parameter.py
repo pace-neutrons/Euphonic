4441ab60bc44f3998eb7c6e8116ce6b1e4a3e16a
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2021-06-15 10:55:32 +0100

    Update CL script and tests

--- euphonic/cli/optimise_dipole_parameter.py
+++ euphonic/cli/optimise_dipole_parameter.py
@@ -1,9 +1,10 @@
 # -*- coding: UTF-8 -*-
 """
-Run and time the Ewald sum part of an interpolation calculation for a small
-number of q-points for different values of eta (this is the balance between
-the real/reciprocal space cutoffs when performing the Ewald sum for polar
-materials) to determine the optimal value
+Run and time the Ewald sum part of an interpolation calculation for a
+small number of q-points for different values of dipole_parameter (this
+is the balance between the real/reciprocal space cutoffs when
+performing the Ewald sum for polar materials) to determine the optimal
+value
 """
 
 import argparse
@@ -17,53 +18,70 @@ def main():
     args = parser.parse_args()
     params = vars(args)
     params.update({'print_to_terminal': True})
-    calculate_optimum_eta(**params)
+    calculate_optimum_dipole_parameter(**params)
 
 
-def calculate_optimum_eta(filename: str, eta_min: float = 0.25, eta_max: float = 1.5, eta_step: float = 0.25,
-                          n: int = 100, print_to_terminal: bool = False):
+def calculate_optimum_dipole_parameter(
+        filename: str,
+        dipole_parameter_min: float = 0.25,
+        dipole_parameter_max: float = 1.5,
+        dipole_parameter_step: float = 0.25,
+        n: int = 100,
+        print_to_terminal: bool = False):
     """
-    Calculate the optimum eta and other etas from the filename castep_bin file
+    Calculate the optimum dipole_parameter and other dipole_parameters
+    from the filename castep_bin file
 
     Parameters
     ----------
     filename : str
         The path and name of the .castep_bin/.check to read from
-    eta_min : float, optional, Default: 0.25
-        The minimum value of eta to test
-    eta_max : float, optional, Default: 1.5
-        The maximum value of eta to test
-    eta_step : float, optional, Default: 0.25
-        The difference between each eta to test
-    n : int, optional, Default: 100
-        The number of times to loop over q-points. A higher value will get a more reliable timing, but will take longer
-    print_to_terminal : bool, optional, Default: False
+    dipole_parameter_min
+        The minimum value of dipole_parameter to test
+    dipole_parameter_max
+        The maximum value of dipole_parameter to test
+    dipole_parameter_step
+        The difference between each dipole_parameter to test
+    n
+        The number of times to loop over q-points. A higher value will
+        get a more reliable timing, but will take longer
+    print_to_terminal
         Whether to print the outcome to terminal or not
 
     Returns
     -------
     Tuple[float, float, float, np.array, np.array, np.array]:
-        A tuple of the optimal eta, the time it took to initialise the optimal eta,
-        the time per qpoint for the optimal eta, other etas, their initialisation times and their times per qpoint.
+        A tuple of:
+         - the optimal dipole_parameter
+         - the time it took to initialise the optimal dipole_parameter,
+         - the time per qpoint for the optimal dipole_parameter
+         - other tested dipole_parameters
+         - other tested diple_parameters initialisation times
+         - other tested diple_parameters times per q-point
     """
-    etas = np.arange(eta_min, eta_max + eta_step / 100, eta_step)
-    t_init = np.zeros(len(etas), dtype=np.float64)
-    t_tot = np.zeros(len(etas), dtype=np.float64)
+    dipole_parameters = np.arange(
+        dipole_parameter_min,
+        dipole_parameter_max + dipole_parameter_step / 100,
+        dipole_parameter_step)
+    t_init = np.zeros(len(dipole_parameters), dtype=np.float64)
+    t_tot = np.zeros(len(dipole_parameters), dtype=np.float64)
 
     idata = ForceConstants.from_castep(filename)
     sfmt = '{:20s}'
     tfmt = '{: 3.2f}'
-    etafmt = '{: 2.2f}'
-    for i, eta in enumerate(etas):
+    dparamfmt = '{: 2.2f}'
+    for i, dipole_parameter in enumerate(dipole_parameters):
         if print_to_terminal:
-            print(('Results for eta ' + etafmt).format(eta))
+            print(('Results for dipole_parameter ' + dparamfmt).format(
+                dipole_parameter))
         # Time Ewald sum initialisation
         start = time.time()
-        idata._dipole_correction_init(eta_scale=eta)
+        idata._dipole_correction_init(dipole_parameter=dipole_parameter)
         end = time.time()
         t_init[i] = end - start
         if print_to_terminal:
-            print((sfmt + ': ' + tfmt + ' s').format('Ewald init time', t_init[i]))
+            print((sfmt + ': ' + tfmt + ' s').format(
+                'Ewald init time', t_init[i]))
 
         # Time per qpt
         start = time.time()
@@ -72,23 +90,28 @@ def calculate_optimum_eta(filename: str, eta_min: float = 0.25, eta_max: float =
         end = time.time()
         t_tot[i] = end - start
         if print_to_terminal:
-            print((sfmt + ': ' + tfmt + ' ms\n').format('Ewald time/qpt', t_tot[i]*1000/n))
+            print((sfmt + ': ' + tfmt + ' ms\n').format(
+                'Ewald time/qpt', t_tot[i]*1000/n))
 
     opt = np.argmin(t_tot)
     if print_to_terminal:
         print('******************************')
-        print(('Suggested optimum eta is ' + etafmt).format(etas[opt]))
+        print(('Suggested optimum dipole_parameter is ' + dparamfmt).format(
+            dipole_parameters[opt]))
         print((sfmt + ': ' + tfmt + ' s').format('init time', t_init[opt]))
-        print((sfmt + ': ' + tfmt + ' ms\n').format('time/qpt', t_tot[opt]*1000/n))
+        print((sfmt + ': ' + tfmt + ' ms\n').format(
+            'time/qpt', t_tot[opt]*1000/n))
 
-    return etas[opt], t_init[opt], t_tot[opt], etas, t_init, t_tot
+    return (dipole_parameters[opt], t_init[opt], t_tot[opt],
+            dipole_parameters, t_init, t_tot)
 
 
 def get_parser():
     parser = argparse.ArgumentParser(
         description=('Run and time an interpolation calculation for a small '
-                     'number of q-points for different values of eta to '
-                     'determine eta\'s optimum value for this material'))
+                     'number of q-points for different values of '
+                     'dipole_parameter to determine dipole_parameter\'s '
+                     'optimum value for this material'))
     parser.add_argument(
         'filename',
         help='The .castep_bin file to extract the data from')
@@ -96,26 +119,27 @@ def get_parser():
         '-n',
         default=100,
         type=int,
-        help=('The number of times to loop over q-points. A higher value will '
-              'get a more reliable timing, but will take longer')
+        help=('The number of times to loop over q-points. A higher '
+              'value will get a more reliable timing, but will take '
+              'longer')
     )
     parser.add_argument(
-        '--eta_min',
+        '--dipole_parameter_min',
         default=0.25,
         type=float,
-        help='The minimum value of eta to test'
+        help='The minimum value of dipole_parameter to test'
     )
     parser.add_argument(
-        '--eta_max',
+        '--dipole_parameter_max',
         default=1.5,
         type=float,
-        help='The maximum value of eta to test'
+        help='The maximum value of dipole_parameter to test'
     )
     parser.add_argument(
-        '--eta_step',
+        '--dipole_parameter_step',
         default=0.25,
         type=float,
-        help='The difference between each eta to test'
+        help='The difference between each dipole_parameter to test'
     )
 
     return parser
--- tests_and_analysis/test/script_tests/test_optimise_dipole_parameter.py
+++ tests_and_analysis/test/script_tests/test_optimise_dipole_parameter.py
@@ -6,7 +6,7 @@ from random import random
 import time
 from unittest.mock import Mock
 from euphonic import ForceConstants
-from euphonic.cli.optimise_eta import calculate_optimum_eta
+from euphonic.cli.optimise_dipole_parameter import calculate_optimum_dipole_parameter
 from tests_and_analysis.test.utils import get_castep_path
 
 
@@ -16,22 +16,24 @@ quartz_castep_bin = get_castep_path("quartz", "quartz.castep_bin")
 class SharedCode:
 
     @staticmethod
-    def get_lowest_time_per_qpt_and_index(etas_time_per_qpts):
-        # Search etas_time_per_qpts for lowest time
+    def get_lowest_time_per_qpt_and_index(dipole_parameters_time_per_qpts):
+        # Search dipole_parameters_time_per_qpts for lowest time
         lowest_time_per_qpt = math.inf
         lowest_time_per_qpt_index = None
-        for index, time_per_qpt in enumerate(etas_time_per_qpts):
+        for index, time_per_qpt in enumerate(dipole_parameters_time_per_qpts):
             if time_per_qpt < lowest_time_per_qpt:
                 lowest_time_per_qpt = time_per_qpt
                 lowest_time_per_qpt_index = index
         return lowest_time_per_qpt, lowest_time_per_qpt_index
 
     @staticmethod
-    def get_calculate_optimum_eta_kwargs():
+    def get_calculate_optimum_dipole_parameter_kwargs():
         return [
             {},
-            {"eta_min": 0.25, "eta_max": 1.35, "eta_step": 0.2, "n": 20},
-            {"eta_min": 0.5, "eta_max": 1.75, "eta_step": 0.3, "n": 10}
+            {"dipole_parameter_min": 0.25, "dipole_parameter_max": 1.35,
+             "dipole_parameter_step": 0.2, "n": 20},
+            {"dipole_parameter_min": 0.5, "dipole_parameter_max": 1.75,
+             "dipole_parameter_step": 0.3, "n": 10}
         ]
 
 
@@ -49,7 +51,8 @@ class MockTime:
 class TestUnit:
 
     # Run the fixture for multiple permutations of arguments
-    @pytest.fixture(params=SharedCode.get_calculate_optimum_eta_kwargs())
+    @pytest.fixture(
+        params=SharedCode.get_calculate_optimum_dipole_parameter_kwargs())
     def call_with_params_and_quartz_unit(self, request, monkeypatch):
         # Mock getting the data from a castep file
         force_constants_mock = Mock()
@@ -62,24 +65,26 @@ class TestUnit:
             time, "time", lambda *args, **kwargs: time_manager.get_time())
 
         kwargs = request.param
-        return calculate_optimum_eta(quartz_castep_bin, **kwargs)
+        return calculate_optimum_dipole_parameter(quartz_castep_bin, **kwargs)
 
     def test_optimal_has_lowest_time_per_qpt(
             self, call_with_params_and_quartz_unit):
         # Unpack data
-        optimal_eta = call_with_params_and_quartz_unit[0]
+        optimal_dipole_parameter = call_with_params_and_quartz_unit[0]
         optimal_time_per_qpt = call_with_params_and_quartz_unit[2]
-        etas = call_with_params_and_quartz_unit[3]
-        etas_time_per_qpts = call_with_params_and_quartz_unit[5]
+        dipole_parameters = call_with_params_and_quartz_unit[3]
+        dipole_parameters_time_per_qpts = call_with_params_and_quartz_unit[5]
 
         lowest_time_per_qpt, lowest_time_per_qpt_index = \
-            SharedCode.get_lowest_time_per_qpt_and_index(etas_time_per_qpts)
+            SharedCode.get_lowest_time_per_qpt_and_index(
+                dipole_parameters_time_per_qpts)
 
         # Check optimal time per q-point is the lowest detected q-point
         assert optimal_time_per_qpt == lowest_time_per_qpt
-        # Check eta from lowest detected q-point index matches our
-        # optimal q-point
-        assert etas[lowest_time_per_qpt_index] == optimal_eta
+        # Check dipole_parameter from lowest detected q-point index matches
+        # our optimal q-point
+        assert dipole_parameters[
+            lowest_time_per_qpt_index] == optimal_dipole_parameter
 
 
 @pytest.mark.integration
@@ -87,36 +92,41 @@ class TestRegression:
 
     @pytest.fixture
     def call_with_defaults_and_quartz_integration(self):
-        return calculate_optimum_eta(quartz_castep_bin)
+        return calculate_optimum_dipole_parameter(quartz_castep_bin)
 
     # Regression test
     def test_optimal_is_0_75(self, call_with_defaults_and_quartz_integration):
-        optimal_eta = call_with_defaults_and_quartz_integration[0]
-        assert optimal_eta == 0.75
+        optimal_dipole_parameter = \
+            call_with_defaults_and_quartz_integration[0]
+        assert optimal_dipole_parameter == 0.75
 
 
 @pytest.mark.integration
 class TestIntegration:
 
     # Run the fixture for multiple permutations of arguments
-    @pytest.fixture(params=SharedCode.get_calculate_optimum_eta_kwargs())
+    @pytest.fixture(
+        params=SharedCode.get_calculate_optimum_dipole_parameter_kwargs())
     def call_with_params_and_quartz_integration(self, request):
         kwargs = request.param
-        return calculate_optimum_eta(quartz_castep_bin, **kwargs)
+        return calculate_optimum_dipole_parameter(quartz_castep_bin, **kwargs)
 
     def test_optimal_has_lowest_time_per_qpt(
             self, call_with_params_and_quartz_integration):
         # Unpack data
-        optimal_eta = call_with_params_and_quartz_integration[0]
+        optimal_dipole_parameter = call_with_params_and_quartz_integration[0]
         optimal_time_per_qpt = call_with_params_and_quartz_integration[2]
-        etas = call_with_params_and_quartz_integration[3]
-        etas_time_per_qpts = call_with_params_and_quartz_integration[5]
+        dipole_parameters = call_with_params_and_quartz_integration[3]
+        dipole_parameters_time_per_qpts = \
+            call_with_params_and_quartz_integration[5]
 
         lowest_time_per_qpt, lowest_time_per_qpt_index = \
-            SharedCode.get_lowest_time_per_qpt_and_index(etas_time_per_qpts)
+            SharedCode.get_lowest_time_per_qpt_and_index(
+                dipole_parameters_time_per_qpts)
 
         # Check optimal time per q-point is the lowest detected q-point
         assert optimal_time_per_qpt == lowest_time_per_qpt
-        # Check eta from lowest detected q-point index matches our
-        # optimal q-point
-        assert etas[lowest_time_per_qpt_index] == optimal_eta
+        # Check dipole_parameter from lowest detected q-point index matches
+        # our optimal q-point
+        assert dipole_parameters[
+            lowest_time_per_qpt_index] == optimal_dipole_parameter
