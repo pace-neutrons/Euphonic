baaeb3cf40775fad66abebd774b68ff3b0a9e188
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2021-06-22 11:59:24 +0100

    Add simple tests for LICENSE/CITATION.cff install

---
+++ tests_and_analysis/test/euphonic_test/test_install.py
@@ -0,0 +1,17 @@
+from importlib_resources import open_text
+import yaml
+
+import euphonic
+
+
+class TestInstalledFiles:
+
+    def test_license_is_installed(self):
+        with open_text(euphonic, 'LICENSE') as fp:
+            license_data = fp.readlines()
+        assert 'GNU GENERAL PUBLIC LICENSE' in license_data[0]
+
+    def test_citation_cff_is_installed(self):
+        with open_text(euphonic, 'CITATION.cff') as fp:
+            citation_data = yaml.safe_load(fp)
+        assert 'cff-version' in citation_data
