0097e16bda002037337ee0334158341e860a5644
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2021-11-11 16:36:30 +0000

    Add more sensible grid defaults

--- euphonic/brille_interpolator.py
+++ euphonic/brille_interpolator.py
@@ -1,8 +1,9 @@
 import spglib as spg
 import numpy as np
 import brille as br
+from multiprocessing import cpu_count
 
-from euphonic import ureg, QpointPhononModes, DebyeWaller
+from euphonic import ureg, QpointPhononModes
 
 class BrilleInterpolator(object):
     """
@@ -14,41 +15,53 @@ class BrilleInterpolator(object):
     crystal : Crystal
         Lattice and atom information
     """
-    def __init__(self, force_constants, grid_type='trellis', grid_kwargs={},
-                 interp_kwargs={}, n_qpts=1000):
+    def __init__(self, force_constants, grid_type='trellis',
+                 n_grid_points=1000, grid_kwargs=None,
+                 interpolation_kwargs=None):
 
         crystal = force_constants.crystal
         cell_vectors = crystal._cell_vectors
         cell = crystal.to_spglib_cell()
 
         dataset = spg.get_symmetry_dataset(cell)
-        rotations = dataset['rotations'] # in fractional
-        translations = dataset['translations'] # in fractional
+        rotations = dataset['rotations']  # in fractional
+        translations = dataset['translations']  # in fractional
 
         symmetry = br.Symmetry(rotations, translations)
         direct = br.Direct(*cell)
         direct.spacegroup = symmetry
         bz = br.BrillouinZone(direct.star)
 
-        if grid_type == 'mesh':
+        print(f'Generating grid...')
+        if grid_type == 'trellis':
+            if grid_kwargs is None:
+                vol = bz.ir_polyhedron.volume
+                grid_kwargs = {
+                    'node_volume_fraction': vol/n_grid_points}
+            grid = br.BZTrellisQdc(bz, **grid_kwargs)
+        elif grid_type == 'mesh':
+            if grid_kwargs is None:
+                grid_kwargs = {
+                    'max_size': bz.ir_polyhedron.volume/n_grid_points,
+                    'max_points': n_grid_points}
             grid = br.BZMeshQdc(bz, **grid_kwargs)
         elif grid_type == 'nest':
+            if grid_kwargs is None:
+                grid_kwargs = {'number_density': n_grid_points}
             grid = br.BZNestQdc(bz, **grid_kwargs)
-        elif grid_type == 'trellis':
-            if not grid_kwargs:
-                grid_kwargs = {
-                    'node_volume_fraction': bz.ir_polyhedron.volume/n_qpts}
-            grid = br.BZTrellisQdc(bz, **grid_kwargs)
         else:
             raise ValueError(f'Grid type "{grid_type}" not recognised')
 
-        print(f'Grid generated with {len(grid.rlu)} q-points')
-        interp_kwargs['insert_gamma'] = False
-        interp_kwargs['reduce_qpts'] = False
+        print(f'Grid generated with {len(grid.rlu)} q-points. '
+               'Calculating frequencies/eigenvectors...')
+        if interpolation_kwargs is None:
+            interpolation_kwargs = {}
+        interpolation_kwargs['insert_gamma'] = False
+        interpolation_kwargs['reduce_qpts'] = False
         phonons = force_constants.calculate_qpoint_phonon_modes(
-            grid.rlu, **interp_kwargs)
-        print((f'Frequencies/eigenvectors calculated for '
-               f'{len(grid.rlu)} q-points'))
+            grid.rlu, **interpolation_kwargs)
+        # Convert eigenvectors from Cartesian to cell vectors basis
+        # for storage in grid 
         evecs_basis = np.einsum('ba,ijkb->ijka', np.linalg.inv(cell_vectors),
                                 phonons.eigenvectors)
         n_atoms = crystal.n_atoms
@@ -61,13 +74,18 @@ class BrilleInterpolator(object):
         cost_function = 0
         evecs_el = (0, 3*n_atoms, 0, 3, 0, cost_function)
         evecs_weight = (0., 1., 0.)
+        print(f'Filling grid...')
         grid.fill(frequencies, freq_el, freq_weight, evecs, evecs_el,
                   evecs_weight)
         self._grid = grid
         self.crystal = crystal
 
     def calculate_qpoint_phonon_modes(self, qpts, **kwargs):
+        if not kwargs:
+            kwargs = {'useparallel': True, 'threads': cpu_count()} 
         vals, vecs = self._grid.ir_interpolate_at(qpts, **kwargs)
+        # Eigenvectors in grid are stored in cell vectors basis,
+        # convert to Cartesian
         vecs_cart = self._br_evec_to_eu(
             vecs, cell_vectors=self.crystal._cell_vectors)
         frequencies = vals.squeeze()*ureg('hartree').to('meV')
