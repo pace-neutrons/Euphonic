7df05f00d5889ff0e8aa40d89d0506b2b72a8131
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2022-02-24 16:08:49 +0000

    Add SQW and residual plots

--- euphonic/cli/check_brille_settings.py
+++ euphonic/cli/check_brille_settings.py
@@ -4,16 +4,19 @@ import itertools
 from pathlib import Path
 import time
 from typing import List, Tuple, Optional
+import random
 import warnings
 
 import matplotlib.pyplot as plt
 import numpy as np
 
-from euphonic import ForceConstants
+from euphonic import ForceConstants, ureg
 from euphonic.brille import BrilleInterpolator
 from euphonic.cli.utils import (_get_cli_parser, get_args,
                                 _brille_calc_modes_kwargs,
-                                load_data_from_file)
+                                load_data_from_file,
+                                _get_energy_bins)
+from euphonic.plot import plot_1d_to_axis
 from euphonic.sampling import recurrence_sequence
 
 
@@ -31,6 +34,9 @@ def check_brille_settings(
         brille_npts: int = 5000,
         brille_npts_density: Optional[int] = None,
         ftol: Optional[float] = None,
+        n: int = 0,
+        eb: float = 0,
+        ebins: int = 500,
         **calc_modes_kwargs
         ) -> None:
 
@@ -49,8 +55,29 @@ def check_brille_settings(
         sf, freqs_plot = sum_at_degenerate_modes(
             sf, modes.frequencies.magnitude, ftol)
 
+
     fig, ax = plt.subplots()
     fig_sf, ax_sf = plt.subplots(subplot_kw={'projection': '3d'})
+    ebins = _get_energy_bins(modes, ebins)
+    sqw1d = sf_obj.calculate_1d_average(ebins)
+    sqw1d = sqw1d.broaden(eb*ureg('meV'))
+    ebins_plot = sqw1d.get_bin_centres().magnitude
+    # First ax for values, second for residual
+    fig_sqw1d, ax_sqw1d = plt.subplots(2)
+    fig_sqw1d.suptitle('1D Average over all Q-points')
+    ax_sqw1d[0].plot(ebins_plot, sqw1d.y_data.magnitude, label='Euphonic', color='k')
+
+    ax_sqwn = []
+    if n > 0:
+        sqw = sf_obj.calculate_sqw_map(ebins)
+        sqw = sqw.broaden(y_width=eb*ureg('meV'))
+
+        for idx in range(n):
+            fign, axn = plt.subplots(2)
+            fign.suptitle(f'Q={qpts[idx]}')
+            axn[0].plot(ebins_plot, sqw.z_data[idx].magnitude,
+                        label='Euphonic', color='k')
+            ax_sqwn.append(axn)
 
     if brille_npts_density is not None:
         brille_params = brille_npts_density
@@ -80,12 +107,27 @@ def check_brille_settings(
         eps = interp_modes.frequencies.magnitude - modes.frequencies.magnitude
         eps_sf = (interp_sf - sf)
 
+        label = f'{actual_brille_npts} ({actual_brille_density})'
         ax.plot(modes.frequencies.magnitude.flatten(), eps.flatten(), 'x',
-                label=f'{actual_brille_npts} ({actual_brille_density})')
-        ax_sf.plot(freqs_plot.flatten(),
-                   sf.flatten(),
-                   eps_sf.flatten(), 'x',
-                   label=f'{actual_brille_npts} ({actual_brille_density})')
+                label=label)
+        ax_sf.plot(freqs_plot.flatten(), sf.flatten(), eps_sf.flatten(), 'x',
+                   label=label)
+
+        interp_sqw1d = interp_sf_obj.calculate_1d_average(ebins)
+        interp_sqw1d = interp_sqw1d.broaden(eb*ureg('meV'))
+        eps_sqw1d = interp_sqw1d.y_data.magnitude - sqw1d.y_data.magnitude
+        ax_sqw1d[0].plot(ebins_plot, interp_sqw1d.y_data.magnitude,
+                         label=label)
+        ax_sqw1d[1].plot(ebins_plot, eps_sqw1d, label=label)
+        if n is not None:
+            interp_sqwn = interp_sf_obj.calculate_sqw_map(ebins)
+            interp_sqwn = interp_sqwn.broaden(y_width=eb*ureg('meV'))
+            eps_sqwn = interp_sqwn.z_data.magnitude - sqw.z_data.magnitude
+            for idx, ax_sqw in enumerate(ax_sqwn):
+                ax_sqw[0].plot(ebins_plot, interp_sqwn.z_data[idx].magnitude,
+                               label=label)
+                ax_sqw[1].plot(ebins_plot, eps_sqwn[idx],
+                               label=label)
 
     ax.set_xlabel(f'Frequency / {modes.frequencies.units:~P}')
     ax.set_ylabel(f'Epsilon / {modes.frequencies.units:~P}')
@@ -98,6 +140,10 @@ def check_brille_settings(
     ax_sf.set_zlabel(f'Epsilon / {sf_obj.structure_factors.units:~P}')
     ax_sf.legend(title='Brille mesh size (density)')
     ax_sf.set_title(Path(filename).name)
+
+    ax_sqw1d[0].legend()
+    for axi in ax_sqwn:
+        axi[0].legend()
 
     plt.show()
 
@@ -171,5 +217,8 @@ def get_parser() -> ArgumentParser:
               'degenerate. If modes are degenerate their structure '
               'factors will be summed before comparison. If not provided, '
               'structure factors are not summed.'))
+    sections['brille'].add_argument('-n', type=int, default=0)
+    sections['brille'].add_argument('--eb', type=float, default=0)
+    sections['brille'].add_argument('--ebins', type=float, default=500)
 
     return parser
