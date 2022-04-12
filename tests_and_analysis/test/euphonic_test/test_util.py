ed058373280b761d5244a1f0c5bbde410da69158
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2022-04-12 13:38:08 +0100

    Add convert_fc tests

--- tests_and_analysis/test/euphonic_test/test_util.py
+++ tests_and_analysis/test/euphonic_test/test_util.py
@@ -1,5 +1,6 @@
 import json
 import os
+import sys
 
 import pytest
 import numpy as np
@@ -7,11 +8,11 @@ import numpy.testing as npt
 
 from euphonic import ureg
 from euphonic.util import (direction_changed, mp_grid, get_qpoint_labels,
-                           mode_gradients_to_widths)
+                           mode_gradients_to_widths, convert_fc_phases)
 from tests_and_analysis.test.utils import get_data_path
 from tests_and_analysis.test.euphonic_test.test_crystal import get_crystal
 from tests_and_analysis.test.euphonic_test.test_force_constants import (
-    get_fc_path)
+    get_fc_path, get_fc)
 
 
 class TestDirectionChanged:
@@ -103,3 +104,37 @@ class TestModeGradientsToWidths:
         assert mode_widths.units == expected_mode_widths.units
         npt.assert_allclose(mode_widths.magnitude,
                             expected_mode_widths.magnitude, atol=3e-4)
+
+
+def get_data_from_json(filename):
+    with open(get_fc_path(filename), 'r') as fp:
+        data = json.load(fp)
+    fc = np.array(data['force_constants'])*ureg(data['force_constants_unit'])
+    atom_r = np.array(data['atom_r'])
+    sc_atom_r = np.array(data['sc_atom_r'])
+    sc_to_uc_idx = np.array(data['sc_to_uc_idx'])
+    uc_to_sc_idx = np.array(data['uc_to_sc_idx'])
+    sc_matrix = np.array(data['sc_matrix'])
+
+    return fc, atom_r, sc_atom_r, uc_to_sc_idx, sc_to_uc_idx, sc_matrix
+
+
+class TestConvertFcPhases:
+
+    @pytest.mark.parametrize(('fc, atom_r, sc_atom_r, uc_to_sc_idx, '
+                              'sc_to_uc_idx, sc_matrix, expected_fc'), [
+        (*get_data_from_json('NaCl_convert_full_fc_data.json'),
+         get_fc('NaCl')),
+        (*get_data_from_json('CaHgO2_convert_fc_data.json'),
+         get_fc('CaHgO2'))])
+    def test_convert_fc_phases(self, fc, atom_r, sc_atom_r, uc_to_sc_idx,
+                               sc_to_uc_idx, sc_matrix, expected_fc):
+        converted_fc, cell_origins = convert_fc_phases(
+            fc.magnitude, atom_r, sc_atom_r, uc_to_sc_idx, sc_to_uc_idx,
+            sc_matrix)
+        npt.assert_allclose(cell_origins, expected_fc.cell_origins)
+        expected_fc_unit = expected_fc.force_constants.units
+        npt.assert_allclose(
+            (converted_fc*fc.units).to(expected_fc_unit).magnitude,
+            expected_fc.force_constants.magnitude,
+            atol=sys.float_info.epsilon)
