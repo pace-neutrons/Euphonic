11bd595e2f510d47ccf63c34e11b1bbf016323ac
Rebecca Fair <rebecca.fair@stfc.ac.uk>
2023-01-26 17:02:46 +0000

    Small improvements to BrilleInterpolator

--- euphonic/brille.py
+++ euphonic/brille.py
@@ -31,7 +31,16 @@ class BrilleInterpolator:
     def __init__(self, crystal: Crystal,
                  grid: Union[br.BZTrellisQdc, br.BZMeshQdc,
                              br.BZNestQdc]) -> None:
-
+        """
+        Parameters
+        ----------
+        crystal
+            Lattice and atom information
+        grid
+            A Brille grid filled with phonon eigenvectors in
+            Cartesian coordinates and phonon frequencies in meV. See
+            Brille documentation for details.
+        """
         _check_constructor_inputs(
             [crystal, grid],
             [Crystal, [br.BZTrellisQdc, br.BZMeshQdc, br.BZNestQdc]],
@@ -59,12 +68,12 @@ class BrilleInterpolator:
         ----------
         qpts
             Shape (n_qpts, 3) float ndarray. The q-points to
-            interpolate onto in reciprocal cell vector units
+            interpolate onto in reciprocal cell vector units.
         **kwargs
             Will be passed to the
             brille.BZTrellis/Mesh/Nest.ir_interpolate_at
             method. By default useparallel=True and
-            threads=multiprocessing.cpu_count() are passed
+            threads=multiprocessing.cpu_count() are passed.
 
         Returns
         -------
@@ -72,13 +81,9 @@ class BrilleInterpolator:
             An object containing frequencies and eigenvectors
             linearly interpolated at each q-point
         """
-        vals, vecs = self._br_grid_calculate_phonons(
-            self._grid, qpts, **kwargs)
-        frequencies = vals.squeeze(axis=-1)*ureg('hartree').to('meV')
-        n_branches = vecs.shape[1]
-        vecs = vecs.view().reshape(-1, n_branches, n_branches//3, 3)
+        vals, vecs = self._br_grid_calculate_phonons(qpts, **kwargs)
         return QpointPhononModes(
-            self.crystal, qpts, frequencies, vecs)
+            self.crystal, qpts, vals, vecs)
 
     def calculate_qpoint_frequencies(self, qpts: np.ndarray, **kwargs
                                       ) -> QpointFrequencies:
@@ -90,12 +95,12 @@ class BrilleInterpolator:
         ----------
         qpts
             Shape (n_qpts, 3) float ndarray. The q-points to
-            interpolate onto in reciprocal cell vector units
+            interpolate onto in reciprocal cell vector units.
         **kwargs
             Will be passed to the
             brille.BZTrellis/Mesh/Nest.ir_interpolate_at
             method. By default useparallel=True and
-            threads=multiprocessing.cpu_count() are passed
+            threads=multiprocessing.cpu_count() are passed.
 
         Returns
         -------
@@ -103,17 +108,23 @@ class BrilleInterpolator:
             An object containing frequencies linearly interpolated
             at each q-point
         """
-        vals, _ = self._br_grid_calculate_phonons(self._grid, qpts, **kwargs)
-        frequencies = vals.squeeze(axis=-1)*ureg('hartree').to('meV')
+        vals, _ = self._br_grid_calculate_phonons(qpts, **kwargs)
         return QpointFrequencies(
-            self.crystal, qpts, frequencies)
+            self.crystal, qpts, vals)
 
-    @staticmethod
-    def _br_grid_calculate_phonons(grid, qpts, **kwargs):
+    def _br_grid_calculate_phonons(self, qpts, **kwargs):
+        """
+        Pass kwargs to Brille grid ir_interpolate_at and transform
+        output into Euphonic format
+        """
         qpts = np.ascontiguousarray(qpts)
         if not kwargs:
             kwargs = {'useparallel': True, 'threads': cpu_count()}
-        return grid.ir_interpolate_at(qpts, **kwargs)
+        vals, vecs = self._grid.ir_interpolate_at(qpts, **kwargs)
+        vals = vals.squeeze(axis=-1)*ureg('hartree').to('meV')
+        n_atoms = self.crystal.n_atoms
+        vecs = vecs.view().reshape(-1, 3*n_atoms, n_atoms, 3)
+        return vals, vecs
 
     @classmethod
     def from_force_constants(
@@ -242,7 +253,7 @@ class BrilleInterpolator:
                            (*phonons.eigenvectors.shape[:2], 3*n_atoms))
         n_elems = (0, 3*n_atoms, 0) # num of scalar, vector, matrix elements
         rotates_like = 2 # rotates like gamma
-        length_unit = 1 # angstrom units
+        length_unit = 1 # angstrom units, i.e. Cartesian eigenvectors
         evecs_el = (*n_elems, rotates_like, length_unit)
         evecs_weight = (0., 1., 0.)
         print('Filling grid...')
