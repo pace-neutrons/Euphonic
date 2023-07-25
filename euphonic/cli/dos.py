ad53356144b27d5c1eb6cfab2409367f316504b2
Adam J. Jackson <a.j.jackson@physics.org>
2023-07-25 14:22:17 +0100

    Use cheby-log fit for CLI-DOS instrumental broadening if no adaptive
    This logic case only applies when there is no adaptive broadening, so
    backward compabibility is not an issue.

--- euphonic/cli/dos.py
+++ euphonic/cli/dos.py
@@ -115,7 +115,7 @@ def main(params: Optional[List[str]] = None) -> None:
                           shape=args.shape,
                           method='convolve',
                           width_interpolation_error=args.adaptive_error,
-                          fit='cubic')
+                          fit='cheby-log')
 
     elif args.inst_broadening:
         # Fixed-width broadening
