a2fdf3b2323c327ddb3ebed1dccdf40c36770bf5
Adam J. Jackson <a.j.jackson@physics.org>
2025-01-23 11:09:44 +0000

    Sort out spectra subpackage imports

---
+++ euphonic/spectra/__init__.py
@@ -0,0 +1,19 @@
+from .base import Spectrum1D, Spectrum2D, apply_kinematic_constraints
+from .collections import Spectrum1DCollection, Spectrum2DCollection
+
+from .base import CallableQuantity, XTickLabels
+from .collections import OneLineData, LineData, Metadata
+
+
+__all__ = [
+    "Spectrum1D",
+    "Spectrum2D",
+    "Spectrum1DCollection",
+    "Spectrum2DCollection",
+    "apply_kinematic_constraints",
+    "CallableQuantity",
+    "XTickLabels",
+    "OneLineData",
+    "LineData",
+    "Metadata",
+]
