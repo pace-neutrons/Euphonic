4cb60713d8e22d6a578188e6a78a3d952ea7dff0
Adam J. Jackson <a.j.jackson@physics.org>
2025-05-06 17:04:09 +0100

    Improve test coverage: cli utils (phonopy loader)

--- tests_and_analysis/test/euphonic_test/test_cli_utils.py
+++ tests_and_analysis/test/euphonic_test/test_cli_utils.py
@@ -3,8 +3,9 @@
 
 import pytest
 
-from euphonic.cli.utils import _get_q_distance
+from euphonic.cli.utils import _get_q_distance, _load_phonopy_file
 from euphonic.ureg import Quantity
+from tests_and_analysis.test.utils import get_data_path
 
 
 def test_get_q_distance():
@@ -14,3 +15,11 @@ def test_get_q_distance():
 
     with pytest.raises(ValueError, match='Length unit not known'):
         _get_q_distance('elephant', 4)
+
+def test_load_phonopy_error():
+    fc_file = get_data_path(
+        'phonopy_files', 'CaHgO2', 'full_force_constants.hdf5')
+
+    with pytest.raises(
+            ValueError, match="must be accompanied by phonopy.yaml"):
+        _load_phonopy_file(fc_file)
