3f5e55f624ed967956f29e0c24f29f348709f415
Adam J. Jackson <a.j.jackson@physics.org>
2025-05-08 09:59:42 +0100

    Unit test CLI arg parser constructor; sort cli.utils tests

--- tests_and_analysis/test/euphonic_test/test_cli_utils.py
+++ tests_and_analysis/test/euphonic_test/test_cli_utils.py
@@ -4,6 +4,7 @@
 import pytest
 
 from euphonic.cli.utils import (
+    _get_cli_parser,
     _get_energy_bins,
     _get_q_distance,
     _load_phonopy_file,
@@ -11,6 +12,24 @@ from euphonic.cli.utils import (
 )
 from euphonic.ureg import Quantity
 from tests_and_analysis.test.utils import get_data_path
+
+
+def test_get_cli_parser_error():
+    with pytest.raises(
+            ValueError, match='No band-data-only tools have been defined.'):
+        _get_cli_parser(features={'read-modes'})
+
+    with pytest.raises(
+            ValueError,
+            match='"adaptive-broadening" cannot be applied without "ebins"'):
+        _get_cli_parser(features={'adaptive-broadening'})
+
+
+def test_get_energy_bins_error():
+    with pytest.raises(
+            ValueError,
+            match='Maximum energy should be greater than minimum.'):
+        _get_energy_bins(modes=None, n_ebins=0, emin=1, emax=0)
 
 
 def test_get_q_distance():
@@ -31,12 +50,6 @@ def test_load_phonopy_errors():
             ValueError, match='must be accompanied by phonopy.yaml'):
         _load_phonopy_file(fc_file)
 
-
-def test_get_energy_bins_error():
-    with pytest.raises(
-            ValueError,
-            match='Maximum energy should be greater than minimum.'):
-        _get_energy_bins(modes=None, n_ebins=0, emin=1, emax=0)
 
 def test_load_data_extension_error():
     with pytest.raises(
