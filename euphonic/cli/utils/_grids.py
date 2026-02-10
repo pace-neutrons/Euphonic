65c6f738e2817598e6b0dcb473b5042f9a2ee37f
Adam J. Jackson <a.j.jackson@physics.org>
2026-02-10 20:53:58 +0000

    Refactor CLI utils module (#481)
    Split euphonic.cli.utils to submodules
    
    This file is way too large and hard to navigate
    
    Factor out remaining functions from cli/utils/__init__.py
    
    At this point we may as well finish the job and make this file a
    namespace only.
    
    Remove irrelevant stuff from __all__
    
    This is theoretically a breaking change, but going into 2.0 so no big
    deal. Still it doesn't seem worth advertising?
    
    Update CHANGELOG
    
    Fix mocked class import location
    
    Little bits of cleanup from code review
    
    Early return, reduce indentation
    
    Co-authored-by: Jacob Wilkins <46597752+oerc0122@users.noreply.github.com>
    
    Apply suggestions from code review
    
    Co-authored-by: Jacob Wilkins <46597752+oerc0122@users.noreply.github.com>
    
    More precise type annotation, early-return logic
    
    General principle is to make parameter types permissive, but we _know_
    this is coming from argparse and users aren't supposed to play with it.
    
    Ruff
    
    Better use of pathlib
    
    Update euphonic/cli/utils/_band_structure.py
    
    Co-authored-by: Jacob Wilkins <46597752+oerc0122@users.noreply.github.com>
    
    Ruff

---
+++ euphonic/cli/utils/_grids.py
@@ -0,0 +1,73 @@
+"""Get sensible bins/grids from minimal user input"""
+
+
+import numpy as np
+from pint import UndefinedUnitError
+
+from euphonic import (
+    Crystal,
+    QpointFrequencies,
+    QpointPhononModes,
+    Quantity,
+    ureg,
+)
+from euphonic.util import (
+    format_error,
+)
+
+
+def _get_q_distance(length_unit_string: str, q_distance: float) -> Quantity:
+    """
+    Parse user arguments to obtain reciprocal-length spacing Quantity
+    """
+    try:
+        length_units = ureg(length_unit_string)
+    except UndefinedUnitError as err:
+        msg = format_error(
+            'Length unit not known',
+            reason='Euphonic uses Pint for units.',
+            fix=("Try 'angstrom' or 'bohr'. "
+                 "Metric prefixes are also allowed, e.g 'nm'."),
+        )
+        raise ValueError(msg) from err
+    recip_length_units = 1 / length_units
+    return q_distance * recip_length_units
+
+
+def _get_energy_bins(
+        modes: QpointPhononModes | QpointFrequencies,
+        n_ebins: int, emin: float | None = None,
+        emax: float | None = None,
+        headroom: float = 1.05) -> Quantity:
+    """
+    Gets recommended energy bins, in same units as modes.frequencies.
+    emin and emax are assumed to be in the same units as
+    modes.frequencies, if not provided the min/max values of
+    modes.frequencies are used to find the bin limits
+    """
+    if emin is None:
+        # Subtract small amount from min frequency - otherwise due to unit
+        # conversions binning of this frequency can vary with different
+        # architectures/lib versions, making it difficult to test
+        emin_room = 1e-5*ureg('meV').to(modes.frequencies.units).magnitude
+        emin = min(np.min(modes.frequencies.magnitude - emin_room), 0.)
+    if emax is None:
+        emax = np.max(modes.frequencies.magnitude) * headroom
+    if emin >= emax:
+        msg = format_error(
+            'Maximum energy should be greater than minimum.',
+            fix='Check --e-min and --e-max arguments.',
+        )
+        raise ValueError(msg)
+    return np.linspace(emin, emax, n_ebins) * modes.frequencies.units
+
+
+def _grid_spec_from_args(crystal: Crystal,
+                           grid: list[int] | None = None,
+                           grid_spacing: Quantity = 0.1 * ureg('1/angstrom'),
+                           ) -> tuple[int, int, int]:
+    """Get Monkorst-Pack mesh divisions from user arguments"""
+    if grid:
+        return tuple(grid)
+
+    return crystal.get_mp_grid_spec(spacing=grid_spacing)
