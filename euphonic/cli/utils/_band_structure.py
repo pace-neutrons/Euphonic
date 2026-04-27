1e4ef44980e54e58785dabbe4780e85d234ebc6d
Adam J. Jackson <adam.jackson@stfc.ac.uk>
2026-04-27 16:05:55 +0100

    Update euphonic/cli/utils/_band_structure.py
    Co-authored-by: Jacob Wilkins <46597752+oerc0122@users.noreply.github.com>

--- euphonic/cli/utils/_band_structure.py
+++ euphonic/cli/utils/_band_structure.py
@@ -97,9 +97,12 @@ XTickLabels = list[tuple[int, str]]
 SplitArgs = dict[str, Any]
 
 
-# Dictionary returned by seekpath.get_explicit_k_path_orig_cell
-# Not a complete specification, but these are the parts we care about.
+
 class BandpathDict(TypedDict, total=False):
+   """Dictionary returned by seekpath.get_explicit_k_path_orig_cell.
+
+    Not a complete specification, but these are the parts we care about."""
+    
     explicit_kpoints_labels: Sequence[str]
     explicit_kpoints_rel: Iterable[float]
     is_supercell: bool
