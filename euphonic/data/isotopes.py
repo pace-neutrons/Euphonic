234fcf2bc4305fad844a625551c1ebe113b9f6bb
Adam J. Jackson <a.j.jackson@physics.org>
2026-05-27 17:03:06 +0100

    Begin Sears-1992 CSV data implementation
    In principle record array implementation has some nice advantages:
    - efficient lookup/comparison operations
    - small memory footprint for data
    - can pass around slices rather than copy data
    - column headers are local to data
    
    However there are some downsides:
    - string handling is a little awkward. We don't want to constrain the
      string field width in CSV; the workaround is to put strings in
      "object" (i.e. pointer) fields.
    - Handling of null/missing/invalid values is a bit limited; they all
      get cast to NaN at the moment. In principle we can choose -Inf and
      Inf as other float sentries. We also need a sentry for missing
      a_number in mixed-isotope fields; for now we adopt 0 but it's not
      very elegant!
    
    Another issue is that we currently annotate the the IsotopeData
    methods to only return Quantity; so string fields are an invalid
    target and logic is required to handle this.
    
    Then we wrap in Quantity. This a bit silly for dimensionless int like
    z_number and a_number. But they are nice to have in get_item() for
    diagnostic purposes.
    
    - get_array not implemented yet
    - probably still some redundancy to clean up

--- euphonic/data/isotopes.py
+++ euphonic/data/isotopes.py
@@ -1,11 +1,15 @@
 from abc import abstractmethod
+from functools import cached_property
 from importlib.resources import files
 import json
+from math import isnan
 from pathlib import Path
-from typing import Protocol
+import re
+from typing import Any, Protocol
 
 import numpy as np
 from pint import UndefinedUnitError
+from toolz.dicttoolz import valfilter
 
 import euphonic.data
 from euphonic.ureg import Quantity, ureg
@@ -26,8 +30,8 @@ class Structure(Protocol):
 class IsotopeData(Protocol):
     """Interface for data such as neutron scattering length, cross-section
 
-        Implementations may ignore mass or use it to distinguish between
-        isotopes.
+    Implementations may ignore mass or use it to distinguish between
+    isotopes.
     """
 
     def get_value(self, symbol: str, mass: float, key: str) -> Quantity:
@@ -169,6 +173,7 @@ class LegacyJsonData(AtomTypeDictData):
     Only the atom_type attribute is used to select data from the structure:
     this is not very robust in case of isotopic substitutions.
     """
+
     def __init__(self, collection: str):
         self._collection = collection
 
@@ -185,6 +190,308 @@ class LegacyJsonData(AtomTypeDictData):
             )
             raise KeyError(msg)
         return super().get_array(structure, key)
+
+
+class CsvData(IsotopeData):
+    def __init__(self, csv_file: Path, property_map: dict[str, str]) -> None:
+        """Isotope data collection from Sears-like CSV table
+
+        The format consists of comma-separated fields. The first line should
+        be a header with column labels, including units in parentheses as
+        appropriate, e.g.:
+
+          symbol,z_number,a_number,mass (amu),b_c (fm)
+
+        The next row should indicate native python data types, e.g.:
+
+          str,int,int,float,complex
+
+        Standard element mixtures should have no a_number. Monoisotopic
+        elements (or elements that lack data for other isotopes) should be
+        treated as isotopes, including all three properties.  Elements without
+        a standard isotopic mixture (such as Tc) should be expressed as a set
+        of isotopes.
+
+        Internally, this implementation creates a numpy "record array" and
+        queries the data with numpy comparison/indexing features.
+
+        Parameters
+        ----------
+
+        csv_file: reference data set, such as sears-1992.csv
+        property_columns: mapping of property names to column headers in CSV.
+            The property names *must* include symbol, mass and a_number; these
+            are used to select and organise the data.
+
+        property_map: mapping of keys used to access data to column names in
+            original CSV table. e.g. {"coherent_scattering_length": "b_c"} By
+            default the original column names are used (with units stripped),
+            so {} is an acceptable value given the right CSV headers.
+        """
+        self._csv_file = csv_file
+        self._property_map = property_map
+        self._units: dict[str, ureg.Unit] = {}
+
+    def _get_nearest_row(self, symbol: str, mass: float) -> np.recarray:
+        from euphonic.util import format_error
+
+        table, _ = self._table_and_units
+
+        symbol_rows = table[table['symbol'] == symbol]
+        if len(symbol_rows) < 1:
+            if ':' in symbol:  # Variant notation e.g. H:D; try the stem as el
+                return self.get_item(
+                    self, symbol=(symbol.split(':', maxsplit=1)[0])
+                )
+
+            msg = format_error(
+                f'No data found for {symbol!r} in {self._csv_file}.',
+                fix='Ensure symbol corresponds to an element in the table.',
+            )
+            raise ValueError(msg)
+
+        return symbol_rows[np.argmin(np.abs(symbol_rows['mass'] - mass))]
+
+    def _validate_raw_value(self, row: np.recarray, key: str) -> None:
+        from euphonic.util import format_error
+
+        if isnan(raw_value := row[key]):
+            if row.a_number == 0:
+                summary = (
+                    f'Isotopic mixture {row.symbol} has '
+                    f'invalid value for {key!r}'
+                )
+            else:
+                summary = (
+                    f'Isotope {row.symbol}-{row.a_number} has invalid value '
+                    f'for {key!r}'
+                )
+            msg = format_error(
+                summary,
+                fix='Ensure key corresponds to a column in the table.',
+            )
+            raise ValueError(msg)
+
+        if isinstance(raw_value, str):
+            msg = format_error(
+                'This method cannot return str values.',
+                fix=(
+                    'Choose another property, or read '
+                    f'{self._csv_file} by other means.'
+                ),
+            )
+            raise TypeError(msg)
+
+    def get_value(self, symbol: str, mass: float, key: str) -> Quantity:
+        from euphonic.util import format_error
+
+        table, units = self._table_and_units
+        if key not in table.dtype.names:
+            msg = format_error(
+                f'No data found for {key!r} in {self._csv_file}.',
+                fix='Ensure key corresponds to a column in the table.',
+            )
+            raise AttributeError(msg)
+        unit = units[table.dtype.names.index(key)]
+
+        row = self._get_nearest_row(symbol, mass)
+        self._validate_raw_value(row, key)
+
+        return self._apply_unit(row[key], unit)
+
+    def get_array(self, structure: Structure, key: str) -> Quantity:
+        from euphonic.util import zips
+
+        targets = list(zips(structure.atom_type, structure.atom_mass))
+
+        # WIP
+        _ = {
+            (symbol, mass): self.get_value(symbol, mass, key)
+            for (symbol, mass) in targets
+        }
+        return NotImplemented
+
+    def get_item(self, symbol: str, mass: float) -> dict[str, Quantity]:
+        """Get available data for specified species"""
+        from euphonic.util import format_error, zips
+
+        table, units = self._table_and_units
+
+        symbol_rows = table[table['symbol'] == symbol]
+        if len(symbol_rows) < 1:
+            if ':' in symbol:  # Variant notation e.g. H:D; try the stem as el
+                return self.get_item(
+                    self, symbol=(symbol.split(':', maxsplit=1)[0])
+                )
+
+            msg = format_error(
+                f'No data found for {symbol!r} in {self._csv_file}.',
+                fix='Ensure symbol corresponds to an element in the table.',
+            )
+            raise ValueError(msg)
+
+        nearest_row = symbol_rows[
+            np.argmin(np.abs(symbol_rows['mass'] - mass))
+        ]
+
+        values = (
+            self._apply_unit(item, unit)
+            for item, unit in zips(nearest_row, units)
+        )
+
+        return {
+            key: val
+            for (key, val) in zips(table.dtype.names, values)
+            if val is not None
+        }
+
+    @staticmethod
+    def _apply_unit(
+        value: int | float | complex | str, unit: ureg.Unit | None
+    ) -> Quantity | str:
+        """Apply unit to number or make Dimensionless"""
+        match value, unit:
+            case str(), _:
+                return None
+            case _, None:
+                return value * ureg.Unit('dimensionless')
+            case _:
+                return value * unit
+
+    @staticmethod
+    def _split_unit(col_header: str) -> tuple[str, str | None]:
+        from euphonic.util import format_error
+
+        if match := re.match(
+            r"""(.+?)      # Mandatory NAME; any characters, non-greedy
+                               #
+                   \s*         # Any amount of whitespace between NAME and UNIT
+                               #
+                   (           # Begin unit group
+                               #
+                   \(.+\)    # At least one character surrounded by parens
+                               #
+                   )?$         # Optional group must complete the input string;
+                               # this ensures whole line is used despite
+                               # non-greedy NAME.  Otherwise we can get
+                               # 'NAME (UNIT)' -> ('N', None)
+                """,
+            col_header,
+            re.VERBOSE,
+        ):
+            name, unit = match.groups()
+            return name.strip(), unit.strip() if unit else None
+
+        msg = format_error(
+            f'Could not interpret column header {col_header!r}.',
+            fix='Format should be "name (unit)" or "name".',
+        )
+        raise ValueError(msg)
+
+    @staticmethod
+    def _check_valid_types(
+        types_line: list[str], types_map: dict[str, type]
+    ) -> None:
+        from euphonic.util import format_error
+
+        if unknown_types := set(types_line) - set(types_map):
+            msg = format_error(
+                'Not all types from second line of CSV were recognised.',
+                reason=f'Could not interpret types {unknown_types}.',
+                fix=(
+                    'Second line of CSV should contain comma-separated '
+                    '"int", "complex" and "str"'
+                ),
+            )
+            raise ValueError(msg)
+
+    @staticmethod
+    def _normalise_record(line: str, types: list[type]) -> tuple:
+        record = line.strip().split(',')
+
+        # We can't deal with uncertain values, these should become NaN
+        bad_complex = re.compile(r'\(?[±<>].*$')
+        bad_float = re.compile(r'[±<>].*$')
+
+        def _cast(item: str, item_type: type) -> int | float | complex | str:
+            import builtins
+
+            match item, item_type:
+                case '', builtins.int:
+                    return 0
+                case value, builtins.float if not value or bad_float.match(
+                    value
+                ):
+                    return float('NaN')
+                case value, builtins.complex if not value or bad_complex.match(
+                    value
+                ):
+                    return complex(float('NaN'), float('NaN'))
+                case _:
+                    try:
+                        return item_type(item)
+                    except ValueError as err:
+                        raise ValueError(item, item_type) from err
+
+        return tuple(map(_cast, record, types))
+
+    @property
+    def _table(self) -> np.recarray:
+        return self._table_and_units[0]
+
+    @cached_property
+    def _unit_map(self) -> dict[str, ureg.Unit]:
+        from euphonic.util import zips
+
+        def is_not_none(a: Any) -> bool:
+            """In operator from Python 3.14"""
+            return a is not None
+
+        table, units = self._table_and_units
+        unit_map = dict(zips(table.dtype.names, units))
+        return valfilter(is_not_none, unit_map)
+
+    @cached_property
+    def _table_and_units(self) -> tuple[np.recarray, list[ureg.Unit | None]]:
+        from euphonic.util import zips
+
+        types_map = {
+            'int': int,
+            'float': float,
+            'complex': complex,
+            'str': str,
+        }
+
+        # Map of CSV columns to rename: inverse of user-provided map
+        name_map = {value: key for key, value in self._property_map}
+
+        with self._csv_file.open('rt') as fd:
+            col_names = next(fd).strip().split(',')
+            types_line = next(fd).strip().split(',')
+
+            self._check_valid_types(types_line, types_map)
+            types = [types_map[t] for t in types_line]
+
+            col_names, col_units = zips(*map(self._split_unit, col_names))
+
+            col_names = [name_map.get(name, name) for name in col_names]
+
+            records = [self._normalise_record(record, types) for record in fd]
+
+            # Store strings a pointers to avoid truncation
+            types = [(object if t is str else t) for t in types]
+
+            table = np.rec.fromrecords(
+                records,
+                dtype=list(zips(col_names, types)),
+            )
+
+        units = [ureg.Unit(unit) if unit else None for unit in col_units]
+
+        return table, units
+
+
+sears_1992 = CsvData(files(euphonic.data) / 'sears-1992.csv', {})
 
 
 def get_reference_data(
@@ -242,10 +549,13 @@ def get_reference_data(
 
     if physical_property not in data:
         msg = format_error(
-            (f'No such collection {collection!r} '
-             f'with property {physical_property!r}.'),
-            fix=('Available properties for this collection:'
-                 f'{comma_join(data)}.'),
+            (
+                f'No such collection {collection!r} '
+                f'with property {physical_property!r}.'
+            ),
+            fix=(
+                f'Available properties for this collection:{comma_join(data)}.'
+            ),
         )
         raise ValueError(msg)
 
--- tests_and_analysis/test/euphonic_test/test_isotope_data.py
+++ tests_and_analysis/test/euphonic_test/test_isotope_data.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from math import isnan
 
 import numpy as np
 from numpy.testing import assert_allclose
@@ -10,8 +11,9 @@ from euphonic.data.isotopes import (
     IsotopeData,
     LegacyJsonData,
     Structure,
+    sears_1992,
 )
-from euphonic.ureg import Quantity
+from euphonic.ureg import Quantity, ureg
 
 
 def _compare_quantity(a: Quantity, b: Quantity) -> None:
@@ -142,3 +144,88 @@ def test_protocol_get_value() -> None:
     isotope_data = TestIsotopeData()
 
     assert isotope_data.get_value(dummy, 0.0, 'key2') == Quantity(2.0, 'kg')
+
+
+def _assert_equal_quantity_dict(
+    item_1: dict[str, Quantity], item_2: dict[str, Quantity]
+) -> None:
+    assert item_1.keys() == item_2.keys()
+
+    for key, value in item_1.items():
+        if isnan(value.magnitude):
+            assert isnan(item_2[key].magnitude)
+        else:
+            assert value == item_2[key]
+
+
+class TestSears1992CSV:
+    def test_internals(self) -> None:
+        table, units = sears_1992._table_and_units
+
+        assert table.dtype == [
+            ('symbol', object),
+            ('z_number', '<i8'),
+            ('a_number', '<i8'),
+            ('mass', '<f8'),
+            ('spin', object),
+            ('abundance', '<f8'),
+            ('half_life', '<f8'),
+            ('coherent_scattering_length', '<c16'),
+            ('incoherent_scattering_length', '<c16'),
+            ('coherent_cross_section', '<f8'),
+            ('incoherent_cross_section', '<f8'),
+            ('scattering_cross_section', '<f8'),
+            ('absorption_cross_section', '<f8'),
+        ]
+
+        assert None in units
+        assert ureg.Unit('fermi') in units
+
+        assert sears_1992._unit_map['mass'] == ureg.Unit('amu')
+        assert 'spin' not in sears_1992._unit_map
+
+    def test_get_item(self) -> None:
+        # Monisotopic element
+        # Au,79,197,196.966570103,3/2(+),100.0,,(7.63+0j),(-1.84+0j),7.32,0.43,7.75,98.65
+        _assert_equal_quantity_dict(
+            sears_1992.get_item('Au', mass=197.0),
+            {
+                'z_number': Quantity(79, 'dimensionless'),
+                'a_number': Quantity(197, 'dimensionless'),
+                'mass': Quantity(196.966570103, 'amu'),
+                'abundance': Quantity(100.0, 'percent'),
+                'half_life': Quantity(float('nan'), 'year'),
+                'coherent_scattering_length': Quantity(7.63 + 0j, 'fermi'),
+                'incoherent_scattering_length': Quantity(-1.84 + 0j, 'fermi'),
+                'coherent_cross_section': Quantity(7.32, 'barn'),
+                'incoherent_cross_section': Quantity(0.43, 'barn'),
+                'scattering_cross_section': Quantity(7.75, 'barn'),
+                'absorption_cross_section': Quantity(98.65, 'barn'),
+            },
+        )
+
+        # Isotopic mixture
+        # Hg,80,,200.592,,,,(12.692+0j),,20.24,6.6,26.8,372.3
+        _assert_equal_quantity_dict(
+            sears_1992.get_item('Hg', mass=200.7),
+            {
+                'z_number': Quantity(80, 'dimensionless'),
+                'a_number': Quantity(0, 'dimensionless'),
+                'mass': Quantity(200.592, 'amu'),
+                'abundance': Quantity(float('nan'), 'percent'),
+                'half_life': Quantity(float('nan'), 'year'),
+                'coherent_scattering_length': Quantity(12.692 + 0j, 'fermi'),
+                'incoherent_scattering_length': Quantity(
+                    float('nan'), 'fermi'
+                ),
+                'coherent_cross_section': Quantity(20.24, 'barn'),
+                'incoherent_cross_section': Quantity(6.6, 'barn'),
+                'scattering_cross_section': Quantity(26.8, 'barn'),
+                'absorption_cross_section': Quantity(372.3, 'barn'),
+            },
+        )
+
+    def test_get_value(self) -> None:
+        assert sears_1992.get_value(
+            'Hg', mass=200.7, key='coherent_scattering_length'
+             ) == Quantity(12.692+0j, 'fermi')
